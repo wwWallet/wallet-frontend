5bf5b7f02c969c132ba9e7fbe56bb159eef8a734
vmarkop <billy.mark.b.m.10@gmail.com>
2022-10-26 11:34:10 +0300

    Filter VCs settings modal

--- src/components/MyCredentials/MyCredentials.tsx
+++ src/components/MyCredentials/MyCredentials.tsx
@@ -5,12 +5,28 @@ import decode from 'jwt-decode';
 import React, { useEffect, useState } from "react";
 import CredentialList from "../CredentialList/CredentialList";
 import { CredentialEntity } from "../../interfaces/credential.interface";
+import Modal from 'react-modal';
+import './MyCredentials.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faTimes } from "@fortawesome/free-solid-svg-icons";
+import CustomSelect from "../CustomSelect/CustomSelect";
 
 const MyCredentials: React.FC<{ polyglot: Polyglot }> = ({ polyglot }) => {
 
 	const [loading, setLoading] = useState(false);
 	const [message, setMessage] = useState("");
 	const [credentials, setCredentials] = useState<any[]>([]);
+
+
+	// modal
+	const [modalSettings, setModalSettings] = useState<boolean>(false);
+	const handleOpenModal = () => {
+		setModalSettings(true);
+	}
+	const handleCloseModal = () => {
+		setModalSettings(false);
+	}
+	// end modal
 
 	// load credentials from db
 	useEffect(() => {
@@ -53,12 +69,41 @@ const MyCredentials: React.FC<{ polyglot: Polyglot }> = ({ polyglot }) => {
 				:
 				<div>
 					<h4>{polyglot.t('Wallet.tab1.verifiableCredentials')}</h4>
+					<div className='filter-vc'>
+						<span className="hamburger fa fa-cog" onClick={handleOpenModal}/>
+					</div>
 					<CredentialList polyglot={polyglot} credentials={credentials} loaded={loading} />
 					{!credentials.length && !loading &&
 						<div className="message">
 							{polyglot.t('Wallet.tab1.emptyVC')}
 						</div>
 					}
+
+					<Modal
+						className="my-modal"
+						overlayClassName="my-modal-wrapper"
+						isOpen={modalSettings}
+						ariaHideApp={false}
+						onRequestClose={handleCloseModal}
+					>
+						<div className="modal-header">
+							<h4>Filter Credentials</h4>
+							<button type="button" id="close" onClick={handleCloseModal}>
+								<FontAwesomeIcon className="CloseModal" icon={faTimes} />
+							</button>
+						</div>
+						<div className='content'>
+							<div className="filter-vc-container">
+									Credential Type:
+									<CustomSelect items={[{value: 'All', label: 'All'}, {value: 'Europass', label: 'Europass'}]} onChange={ () => {}}/>
+									<button
+										className="small login-button ui fancy button authorize-btn"
+										onClick={ () => {handleCloseModal()} }>
+										Apply
+									</button>
+								</div>
+						</div>
+					</Modal>
 				</div>
 			}
 		</div>
