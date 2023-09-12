63b91bbc23420c65f32b3f418da39b526cc21ed5
Emmanouil Koukoularis <koukoularisman@gmail.com>
2023-09-12 14:44:38 +0300

    handling generateOID proof only to provide structure

--- src/components/useHandleServerMessages.ts
+++ src/components/useHandleServerMessages.ts
@@ -1,8 +1,11 @@
 import { useEffect } from "react";
+import { useLocalStorageKeystore } from "../services/LocalStorageKeystore";
+import { ClientSocketMessage, SignatureAction, WalletKeystoreRequest } from "../types/shared.types";
 
 
 export default function useHandleServerMessages(): {} {
 
+	const keystore = useLocalStorageKeystore();
 	let socket: WebSocket;
 
 	useEffect(() => {
@@ -13,8 +16,27 @@ export default function useHandleServerMessages(): {} {
 			socket.send("First message")
 		});
 
-		socket.addEventListener('message', (event) => {
-			console.log("Message reeceived = ", event.data)
+		socket.addEventListener('message', async (event) => {
+			try {
+				const message = JSON.parse(event.data);
+				const { message_id, request } = message as { message_id: string, request: WalletKeystoreRequest };
+				if (request.action == SignatureAction.generateOpenid4vciProof) {
+					const { proof_jwt } = await keystore.generateOpenid4vciProof(request.audience, request.nonce)
+					console.log("proof jwt = ", proof_jwt);
+					const outgoingMessage: ClientSocketMessage = {
+						message_id: message_id,
+						response: {
+							action: request.action,
+							proof_jwt: proof_jwt
+						}
+					}
+					socket.send(JSON.stringify(outgoingMessage));
+				}
+				console.log("Message reeceived = ", event.data)
+			}
+			catch(e) {
+				console.log('failed to parse message')
+			}
 		})
 
 	}, []);
