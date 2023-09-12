5e9cf0df39f003391339c32c3c67ba725670af63
Emmanouil Koukoularis <koukoularisman@gmail.com>
2023-09-12 14:58:17 +0300

    separated useHandleServerMessages into functions

--- src/components/useHandleServerMessages.ts
+++ src/components/useHandleServerMessages.ts
@@ -8,6 +8,7 @@ export default function useHandleServerMessages(): {} {
 	const keystore = useLocalStorageKeystore();
 	let socket: WebSocket;
 
+
 	useEffect(() => {
 		socket = new WebSocket(`ws://wallet-backend-server:8002`);
 
@@ -16,21 +17,25 @@ export default function useHandleServerMessages(): {} {
 			socket.send("First message")
 		});
 
+		const handleGenerateOpenid4vciProofSigningRequest = async ({ message_id, audience, nonce }: { message_id: string, audience: string, nonce: string}) => {
+			const { proof_jwt } = await keystore.generateOpenid4vciProof(audience, nonce)
+			console.log("proof jwt = ", proof_jwt);
+			const outgoingMessage: ClientSocketMessage = {
+				message_id: message_id,
+				response: {
+					action: SignatureAction.generateOpenid4vciProof,
+					proof_jwt: proof_jwt
+				}
+			}
+			socket.send(JSON.stringify(outgoingMessage));
+		}
+
 		socket.addEventListener('message', async (event) => {
 			try {
 				const message = JSON.parse(event.data);
 				const { message_id, request } = message as { message_id: string, request: WalletKeystoreRequest };
 				if (request.action == SignatureAction.generateOpenid4vciProof) {
-					const { proof_jwt } = await keystore.generateOpenid4vciProof(request.audience, request.nonce)
-					console.log("proof jwt = ", proof_jwt);
-					const outgoingMessage: ClientSocketMessage = {
-						message_id: message_id,
-						response: {
-							action: request.action,
-							proof_jwt: proof_jwt
-						}
-					}
-					socket.send(JSON.stringify(outgoingMessage));
+					handleGenerateOpenid4vciProofSigningRequest({ message_id, ...request });
 				}
 				console.log("Message reeceived = ", event.data)
 			}
