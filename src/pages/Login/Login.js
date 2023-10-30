55658e92514b17f3268320227dcdd0c17b0c7584
Emil Lundberg <emil@yubico.com>
2023-10-30 14:15:35 +0100

    Hide PRF compatibility link while browser support portal is shown

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -482,16 +482,20 @@ const Login = () => {
 					{/* <div className="absolute top-2 right-2">
 						<LanguageSelector />
 					</div> */}
-					<p className="text-sm font-light text-gray-500 dark:text-gray-400 italic mb-2">
-					<FaExclamationTriangle className="text-md inline-block text-orange-600 mr-2" />
-						Learn more about{' '}
-						<a
-							href="https://github.com/wwWallet/wallet-frontend#prf-compatibility" target='blank_'
-							className="font-medium text-custom-blue hover:underline dark:text-blue-500"
-						>
-							PRF compatibility regarding browser support and supported operating systems.
-						</a>
-					</p> 
+					<CheckBrowserSupport.Ctx>
+						<CheckBrowserSupport.IfSupported>
+							<p className="text-sm font-light text-gray-500 dark:text-gray-400 italic mb-2">
+								<FaExclamationTriangle className="text-md inline-block text-orange-600 mr-2" />
+								Learn more about{' '}
+								<a
+									href="https://github.com/wwWallet/wallet-frontend#prf-compatibility" target='blank_'
+									className="font-medium text-custom-blue hover:underline dark:text-blue-500"
+								>
+									PRF compatibility regarding browser support and supported operating systems.
+								</a>
+							</p>
+						</CheckBrowserSupport.IfSupported>
+					</CheckBrowserSupport.Ctx>
 					<div className="p-6 space-y-4 md:space-y-6 sm:p-8 bg-white rounded-lg shadow dark:border">
 						<CheckBrowserSupport.WarningPortal>
 							<h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl text-center dark:text-white">
