23e8a645949da98eed94c8484155899d4e4e96ff
pstamatop <pstamatop@gmail.com>
2023-12-01 12:24:11 +0200

    Add rounded and dark css

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -289,7 +289,7 @@ const WebauthnSignupLogin = ({
 								</button>
 								<button
 									type="button"
-									className="text-white bg-blue-600 text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700"
+									className="text-white bg-blue-600 text-sm px-4 py-2 text-center rounded-lg dark:bg-blue-600 dark:hover:bg-blue-700"
 									onClick={() => resolvePrfRetryPrompt(true)}
 									disabled={prfRetryAccepted}
 								>
@@ -384,7 +384,7 @@ const WebauthnSignupLogin = ({
 						{isLogin && cachedUsers?.length > 0 && <SeparatorLine className="my-4"/>}
 
 						<button
-							className="w-full text-white bg-custom-blue hover:bg-custom-blue-hover dark:text-gray-900 dark:hover:bg-custom-light-blue-hover dark:bg-custom-light-blue focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex flex-row flex-nowrap items-center justify-center"
+							className="w-full text-white bg-custom-blue hover:bg-custom-blue-hover dark:text-gray-900 dark:hover:bg-gray-300 dark:bg-custom-light-blue focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex flex-row flex-nowrap items-center justify-center"
 							type="submit"
 							disabled={isSubmitting}
 						>
