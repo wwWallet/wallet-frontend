8fcc6fb4162781763a23ca9778a74ce68daa363a
gkatrakazas <grigoriskatrakazas@gmail.com>
2023-12-04 19:31:43 +0200

    add get fcm token and notification permission in private router

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,24 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
-
 import * as api from '../api';
-
+import { fetchToken } from '../firebase';
 
 const PrivateRoute = ({ children }) => {
-	const isLoggedIn = api.isLoggedIn();
-	const location = useLocation();
+  const [isPermissionGranted, setIsPermissionGranted] = useState(false);
+  const isLoggedIn = api.isLoggedIn();
+  const location = useLocation();
 
-	// const handleauthrespn{
+  useEffect(() => {
+    const requestNotificationPermission = async () => {
+      console.log(Notification.permission);
+      try {
+        if (Notification.permission !== 'granted') {
+          const permissionResult = await Notification.requestPermission();
+          if (permissionResult === 'granted') {
+            setIsPermissionGranted(true);
+          }
+        } else {
+          setIsPermissionGranted(true);
+        }
+      } catch (error) {
+        console.error('Error requesting notification permission:', error);
+      }
+    };
 
-	// 	//take url req to bck handleAuthorizationResponse
-	// 	//if 200 go to root /
-	// }
+    if (isLoggedIn) {
+      requestNotificationPermission();
+    }
+  }, [isLoggedIn]);
 
-	if (!isLoggedIn) {
-		return <Navigate to="/login" state={{ from: location }} replace />;
-	}
+  useEffect(() => {
+    const sendFcmTokenToBackend = async () => {
+      if (isPermissionGranted) {
+        try {
+          const fcmToken = await fetchToken();
+          console.log('FCM Token:', fcmToken);
+          await api.post('/user/session/fcm_token/add', { fcm_token: fcmToken });
+					console.log('send FCM Token:', fcmToken);
 
-	return children;
+        } catch (error) {
+          console.error('Error sending FCM token to the backend:', error);
+        }
+      }
+    };
+
+    sendFcmTokenToBackend();
+  }, [isPermissionGranted]);
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
 };
 
 export default PrivateRoute;
