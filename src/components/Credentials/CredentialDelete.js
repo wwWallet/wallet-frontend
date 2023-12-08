30b35d1d798b8fcb0f53bd68706ee1ed1942210a
pstamatop <pstamatop@gmail.com>
2023-12-08 18:36:34 +0200

    Change delete message

--- src/components/Credentials/CredentialDelete.js
+++ src/components/Credentials/CredentialDelete.js
@@ -59,7 +59,7 @@ const CredentialDelete = ({ credential }) => {
 								<p className="mb-2 mt-4 text-md">
 									Are you sure you want to delete the <strong> {credential.type.replace(/([A-Z])/g, ' $1')}</strong> credential? 
 									<br></br>
-									If you delete it, <strong>all historical presentations related to this credential will also be deleted</strong>.
+										If you delete it, <strong>all the presentations that include this VC will be removed from your history</strong>.
 								</p>
 								<div className="flex justify-end space-x-2 pt-4">
 									<button className="px-4 py-2 text-gray-900 bg-gray-300 hover:bg-gray-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center" onClick={handleCancelDelete}>
