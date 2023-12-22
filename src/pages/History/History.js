3b2764ce1229eb89e5f94cb9b0021e5b6d435e6b
gkatrakazas <grigoriskatrakazas@gmail.com>
2023-12-22 11:02:56 +0200

    add history page  texts to locale

--- src/pages/History/History.js
+++ src/pages/History/History.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { BiLeftArrow, BiRightArrow } from 'react-icons/bi';
+import { useTranslation } from 'react-i18next';
+
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
@@ -18,6 +20,8 @@ const History = () => {
 	const [isImageModalOpen, setImageModalOpen] = useState(false);
 
 	const [currentSlide, setCurrentSlide] = useState(1);
+
+	const { t } = useTranslation();
 
   const sliderRef = useRef();
 
@@ -72,14 +76,16 @@ const History = () => {
   return (
     <>
       <div className="sm:px-6 w-full">
-        <h1 className="text-2xl mb-2 font-bold text-custom-blue">History</h1>
+        <h1 className="text-2xl mb-2 font-bold text-custom-blue">{t('Common.history')}</h1>
         <hr className="mb-2 border-t border-custom-blue/80" />
         <p className="italic pd-2 text-gray-700">
-          View history of credential transmissions, detailing when and to which verifiers you sent
+					{t('PageHistory.description')}
         </p>
 
 				{history.length === 0 ? (
-          <p className="text-gray-700 mt-4">No history presentations found.</p>
+          <p className="text-gray-700 mt-4">
+						{t('PageHistory.noFound')}
+					</p>
         ) : (
 					<div className="my-4 overflow-auto space-y-2" style={{ maxHeight: '85vh' }}>
 
@@ -106,7 +112,7 @@ const History = () => {
 						{/* Popup content */}
 						<div class="flex items-start justify-between border-b rounded-t dark:border-gray-600">
 							<h2 className="right text-lg font-bold p-2 mb-2 text-custom-blue">
-								Presented Credentials
+								{t('PageHistory.popupTitle')}
 							</h2>
 							<button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" onClick={() => setImageModalOpen(false)}>
 								<svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
