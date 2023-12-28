608fe07b8d01cb6f51bc04e031bfc4a7cd3224bc
gkatrakazas <grigoriskatrakazas@gmail.com>
2023-12-28 15:40:31 +0200

    add texts of not found page to locale

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import logo from '../../assets/images/logo.png';
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 const NotFound = () => {
 	const navigate = useNavigate();
   const [isContentVisible, setIsContentVisible] = useState(false);
+
+	const { t } = useTranslation();
 
 	const handleBackToHome = () => {
 		navigate('/');
@@ -24,20 +27,22 @@ const NotFound = () => {
 					<img className="w-20" src={logo} alt="logo" />
 				</a>
 				<h1 className="text-xl mb-7 font-bold leading-tight tracking-tight text-gray-900 md:text-2xl text-center dark:text-white">
-					wwWallet
+				{t('Common.walletName')}
 				</h1>
 				<div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
 					<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
 						<h1 className="text-2xl font-bold leading-tight tracking-tight text-gray-900 md:text-4xl text-center dark:text-white">
-							Oops, page not found!
+							{t('NotFound.title')}
 						</h1>
 
-						<p className='text-center'>Sorry, the page you're looking for cannot be accessed</p>
+						<p className='text-center'>
+							{t('NotFound.message')}
+						</p>
 						<button
 							onClick={handleBackToHome}
 							className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 						>
-							Back to Home
+							{t('NotFound.homeButton')}
 						</button>
 					</div>
 				</div>
