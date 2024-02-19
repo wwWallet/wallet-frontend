5e98bdad2bc1692db56ef0fa80fbcc239fac95dd
gregory1996 <grigoriskatrakazas@gmail.com>
2024-02-19 17:24:46 +0200

    Fix console method line and file name reporting

--- src/ConsoleBehavior.js
+++ src/ConsoleBehavior.js
@@ -16,17 +16,30 @@ function isMethodAllowed(method) {
 }
 
 function ConsoleBehavior() {
-	const originalConsole = { ...console };
+  const originalConsole = { ...console };
 
-	Object.keys(console).forEach(method => {
-		if (typeof console[method] === 'function') {
-			console[method] = (...args) => {
-				if (isMethodAllowed(method)) {
-					originalConsole[method](...args);
-				}
-			};
-		}
-	});
+  const originalPrepareStackTrace = Error.prepareStackTrace;
+
+  Object.keys(console).forEach(method => {
+    if (typeof console[method] === 'function') {
+      console[method] = (...args) => {
+        if (isMethodAllowed(method)) {
+          Error.prepareStackTrace = (_, stack) => stack;
+          const stack = new Error().stack;
+          Error.prepareStackTrace = originalPrepareStackTrace;
+
+          const callSite = stack[1];
+          if (callSite) {
+            const fileName = callSite.getFileName();
+            const lineNumber = callSite.getLineNumber();
+            args.push(`(at ${fileName}:${lineNumber})`);
+          }
+
+          originalConsole[method].apply(console, args);
+        }
+      };
+    }
+  });
 }
 
 export default ConsoleBehavior;
