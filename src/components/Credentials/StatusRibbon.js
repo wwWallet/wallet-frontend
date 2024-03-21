1ca0e32f6fd6de8b977ce9835a1a316d3150d864
gkatrakazas <grigoriskatrakazas@gmail.com>
2024-03-21 15:29:46 +0200

    Use useTranslation to StatusRibbon.js

--- src/components/Credentials/StatusRibbon.js
+++ src/components/Credentials/StatusRibbon.js
@@ -1,7 +1,10 @@
 // StatusRibbon.js
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 const StatusRibbon = ({ expDate }) => {
+	const { t } = useTranslation();
+
 	const CheckExpired = (expDate) => {
 		const today = new Date();
 		const expirationDate = new Date(expDate);
@@ -10,7 +13,7 @@ const StatusRibbon = ({ expDate }) => {
 
 	return (
 		<div className={`absolute bottom-0 right-0 text-white text-xs py-1 px-3 rounded-tl-lg border-t border-l border-white ${CheckExpired(expDate) ? 'bg-red-500' : 'bg-green-500'}`}>
-			{CheckExpired(expDate) ? 'Expired' : 'Active'}
+			{CheckExpired(expDate) ? `${t('statusRibbon.expired')}` : `${t('statusRibbon.active')}`}
 		</div>
 	);
 };
