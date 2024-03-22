100564ad18cb84f885a011b34aec70d5a023c581
pstamatop <pstamatop@gmail.com>
2024-03-22 14:23:02 +0200

    Remove status ribbon for valid VCs

--- src/components/Credentials/StatusRibbon.js
+++ src/components/Credentials/StatusRibbon.js
@@ -12,8 +12,8 @@ const StatusRibbon = ({ expDate }) => {
 	};
 
 	return (
-		<div className={`absolute bottom-0 right-0 text-white text-xs py-1 px-3 rounded-tl-lg border-t border-l border-white ${CheckExpired(expDate) ? 'bg-red-500' : 'bg-green-500'}`}>
-			{CheckExpired(expDate) ? `${t('statusRibbon.expired')}` : `${t('statusRibbon.active')}`}
+		CheckExpired(expDate) && <div className={`absolute bottom-0 right-0 text-white text-xs py-1 px-3 rounded-tl-lg border-t border-l border-white ${CheckExpired(expDate) ? 'bg-red-500' : 'bg-green-500'}`}>
+			{ t('statusRibbon.expired') }
 		</div>
 	);
 };
