06a789073e1d31a4299d15c71e1dc367f2f50706
gkatrakazas <grigoriskatrakazas@gmail.com>
2024-04-17 20:14:20 +0300

    Refactor the qr button base on GetButton component

--- src/components/Buttons/QRButton.js
+++ src/components/Buttons/QRButton.js
@@ -1,31 +1,23 @@
 import React from 'react';
 import { BsQrCodeScan } from 'react-icons/bs';
+import GetButton from './GetButton';
 
 const QRButton = ({ openQRScanner, isSmallScreen }) => {
 	const isMobile = window.innerWidth <= 480;
-	const buttonClass = "px-2 py-2 mb-2 text-white bg-primary hover:bg-primary-hover focus:ring-4 focus:outline-none focus:ring-primary font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-primary-hover dark:hover:bg-primary-hover dark:focus:ring-primary-hover shadow-md";
 
-	if (isMobile) {
+	if (isSmallScreen) {
 		return (
-			<button
-				className={`fixed z-20 bottom-20 right-5 ${buttonClass}`}
-				onClick={openQRScanner}
-			>
-				<div className="flex items-center">
-					<BsQrCodeScan size={20} className="text-white" />
-				</div>
-			</button>
-		);
-	} else if (isSmallScreen) {
-		return (
-			<button
-				className={buttonClass}
-				onClick={openQRScanner}
-			>
-				<div className="flex items-center">
-					<BsQrCodeScan size={20} className="text-white" />
-				</div>
-			</button>
+			<div className="mb-2">
+				<GetButton
+					content={
+						<BsQrCodeScan size={20} className="text-white" />
+					}
+					onClick={openQRScanner}
+					variant="primary"
+					additionalClassName={`${isMobile ? "fixed z-20 bottom-[85px] right-5" : ""}`}
+				/>
+			</div>
+
 		);
 	}
 
