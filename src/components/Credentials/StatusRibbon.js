5f10bc44293d4e35cee8515194f7f1fc66095401
kkmanos <koukoularisman@gmail.com>
2024-06-19 14:30:29 +0300

    added crl parsing from frontend

--- src/components/Credentials/StatusRibbon.js
+++ src/components/Credentials/StatusRibbon.js
@@ -2,9 +2,12 @@
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { parseCredential } from '../../functions/parseCredential';
+import axios from 'axios';
 
 const StatusRibbon = ({ credential }) => {
 	const { t } = useTranslation();
+
+	const [credentialStatus, setCredentialStatus] = useState(null);
 
 	const [parsedCredential, setParsedCredential] = useState(null);
 
@@ -14,6 +17,19 @@ const StatusRibbon = ({ credential }) => {
 		return expirationDate < today;
 	};
 
+	const CheckRevoked = async () => {
+		const [crlListURL, id] = parsedCredential.credentialStatus.id.split('#');
+		if (typeof crlListURL != 'string' || typeof id != 'string') {
+			return false;
+		}
+
+		const crlResult = await axios.get(crlListURL);
+		const { crl } = crlResult.data;
+		console.log("crl = ", crl)
+		const crlRecord = crl.filter((record) => record.id == id)[0];
+		return (crlRecord && crlRecord.revocation_date != null);
+	};
+
 	useEffect(() => {
 		parseCredential(credential).then((c) => {
 			setParsedCredential(c);
@@ -21,15 +37,43 @@ const StatusRibbon = ({ credential }) => {
 	}, []);
 
 
-	return (
-		<>
-			{parsedCredential && CheckExpired(parsedCredential.expirationDate) &&
-				<div className={`absolute bottom-0 right-0 text-white text-xs py-1 px-3 rounded-tl-lg border-t border-l border-white ${CheckExpired(parsedCredential.expirationDate) ? 'bg-red-500' : 'bg-green-500'}`}>
-					{ t('statusRibbon.expired') }
-				</div>
-			}
-		</>
-	);
+	useEffect(() => {
+		if (parsedCredential != null) {
+			CheckRevoked().then((revocationStatus) => {
+				console.log("status = ", revocationStatus)
+				if (revocationStatus == true) {
+					setCredentialStatus('revoked');
+				}
+				else if (CheckExpired(parsedCredential.expirationDate)) {
+					setCredentialStatus('expired');
+				}
+			});
+		}
+	}, [parsedCredential]);
+
+
+	if (parsedCredential && credentialStatus && credentialStatus == 'revoked') {
+		return (
+			<>
+				{
+					<div className={`absolute bottom-0 right-0 text-white text-xs py-1 px-3 rounded-tl-lg border-t border-l border-white ${credentialStatus == 'revoked' ? 'bg-red-500' : 'bg-green-500'}`}>
+						{ t('statusRibbon.revoked') }
+					</div>
+				}
+			</>
+		);
+	}
+	else if (parsedCredential && credentialStatus && credentialStatus == 'expired') {
+		return (
+			<>
+				{
+					<div className={`absolute bottom-0 right-0 text-white text-xs py-1 px-3 rounded-tl-lg border-t border-l border-white ${credentialStatus == 'expired' ? 'bg-red-500' : 'bg-green-500'}`}>
+						{ t('statusRibbon.expired') }
+					</div>
+				}
+			</>
+		);
+	}
 };
 
 export default StatusRibbon;
