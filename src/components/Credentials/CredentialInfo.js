de53c4339053b2dbd864fd5ff825444007f05c1d
gregory1996 <grigoriskatrakazas@gmail.com>
2024-07-01 14:35:46 +0300

    Update icons of vc info

--- src/components/Credentials/CredentialInfo.js
+++ src/components/Credentials/CredentialInfo.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { BiSolidCategoryAlt, BiSolidUserCircle } from 'react-icons/bi';
+import { BiSolidCategoryAlt, BiSolidUserCircle, BiWorld, BiSolidInstitution } from 'react-icons/bi';
 import { AiFillCalendar } from 'react-icons/ai';
 import { RiPassExpiredFill } from 'react-icons/ri';
-import { MdTitle, MdGrade, MdOutlineNumbers } from 'react-icons/md';
+import { MdTitle, MdGrade, MdOutlineNumbers, MdPlace } from 'react-icons/md';
 import { GiLevelEndFlag } from 'react-icons/gi';
 import { formatDate } from '../../functions/DateFormat';
 import { parseCredential } from '../../functions/parseCredential';
@@ -26,6 +26,12 @@ const getFieldIcon = (fieldName) => {
 			return <GiLevelEndFlag size={25} className="inline mr-1" />;
 		case 'grade':
 			return <MdGrade size={25} className="inline mr-1" />;
+		case 'country':
+			return <BiWorld size={25} className="inline mr-1" />;
+		case 'institution':
+			return <BiSolidInstitution size={25} className="inline mr-1" />;
+		case 'place':
+			return <MdPlace size={25} className="inline mr-1" />;
 		default:
 			return null;
 	}
@@ -86,19 +92,19 @@ const CredentialInfo = ({ credential, mainClassName = "text-xs sm:text-sm md:tex
 									{renderRow('id', 'SSN', parsedCredential.credentialSubject.socialSecurityIdentification?.ssn)}
 									{renderRow('id', 'Document ID', parsedCredential.credentialSubject.documentId)}
 									{renderRow('id', 'Institution ID', parsedCredential.credentialSubject.competentInstitution?.competentInstitutionId)}
-									{renderRow('id', 'Institution Name', parsedCredential.credentialSubject.competentInstitution?.competentInstitutionName)}
-									{renderRow('id', 'Institution Country', parsedCredential.credentialSubject.competentInstitution?.competentInstitutionCountryCode)}
+									{renderRow('institution', 'Institution Name', parsedCredential.credentialSubject.competentInstitution?.competentInstitutionName)}
+									{renderRow('country', 'Institution Country', parsedCredential.credentialSubject.competentInstitution?.competentInstitutionCountryCode)}
 								</>
 							)}
 							{parsedCredential.name === "PDA1" && (
 								<>
 									{renderRow('id', 'MS Legislation', parsedCredential.credentialSubject.decisionOnApplicableLegislation?.decisionOnMSWhoseLegislationApplies.memberStateWhoseLegislationIsToBeApplied)}
-									{renderRow('id', 'Employer Name', parsedCredential.credentialSubject.employer?.name)}
-									{renderRow('id', 'Employer Country', parsedCredential.credentialSubject.employer?.countryCode)}
-									{renderRow('id', 'Place of Work', parsedCredential.credentialSubject.placeOfWork?.companyName)}
-									{renderRow('id', 'Town', parsedCredential.credentialSubject.placeOfWork?.town)}
-									{renderRow('id', 'postal Code', parsedCredential.credentialSubject.placeOfWork?.postalCode)}
-									{renderRow('id', 'countryCode', parsedCredential.credentialSubject.placeOfWork?.countryCode)}
+									{renderRow('familyName', 'Employer Name', parsedCredential.credentialSubject.employer?.name)}
+									{renderRow('country', 'Employer Country', parsedCredential.credentialSubject.employer?.countryCode)}
+									{renderRow('place', 'Place of Work', parsedCredential.credentialSubject.placeOfWork?.companyName)}
+									{renderRow('place', 'Town', parsedCredential.credentialSubject.placeOfWork?.town)}
+									{renderRow('place', 'postal Code', parsedCredential.credentialSubject.placeOfWork?.postalCode)}
+									{renderRow('country', 'countryCode', parsedCredential.credentialSubject.placeOfWork?.countryCode)}
 								</>
 							)}
 						</>
