b6fa0276ca02253a5335b144fad2f69a50ed0b15
Emil Lundberg <emil@yubico.com>
2024-07-26 12:22:58 +0200

    Inline function makeGetOptions

--- src/api/LocalAuthentication.ts
+++ src/api/LocalAuthentication.ts
@@ -1,23 +1,18 @@
 export const LocalAuthentication = () => {
 	const rpId = process.env.REACT_APP_WEBAUTHN_RPID;
 
-	const makeGetOptions = (challenge: Uint8Array) => {
-		return {
-			publicKey: {
-				rpId: rpId,
-				challenge: challenge,
-				allowCredentials: [],
-				userVerification: "required",
-			},
-		};
-	}
-
 	return {
 		loginWebAuthnBeginOffline: async (): Promise<{ getOptions: any }> => {
-			// Throwaway challenge, we won't actually verify this for offline login
-			const getOptions = makeGetOptions(window.crypto.getRandomValues(new Uint8Array(32)));
 			return {
-				getOptions: getOptions
+				getOptions: {
+					publicKey: {
+						rpId: rpId,
+						// Throwaway challenge, we won't actually verify this for offline login
+						challenge: window.crypto.getRandomValues(new Uint8Array(32)),
+						allowCredentials: [],
+						userVerification: "required",
+					},
+				},
 			}
 		},
 	}
