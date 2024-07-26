febd282a9894097f226dcfb1d215b0714d1c6506
Emil Lundberg <emil@yubico.com>
2024-07-26 12:19:35 +0200

    Remove unnecessary object wrapping

--- src/api/LocalAuthentication.ts
+++ src/api/LocalAuthentication.ts
@@ -1,11 +1,7 @@
 export const LocalAuthentication = () => {
 	const rpId = process.env.REACT_APP_WEBAUTHN_RPID;
 
-	const makeGetOptions = ({
-		challenge,
-	}: {
-		challenge: Uint8Array,
-	}) => {
+	const makeGetOptions = (challenge: Uint8Array) => {
 		return {
 			publicKey: {
 				rpId: rpId,
@@ -18,10 +14,8 @@ export const LocalAuthentication = () => {
 
 	return {
 		loginWebAuthnBeginOffline: async (): Promise<{ getOptions: any }> => {
-			const getOptions = makeGetOptions({
-				// Throwaway challenge, we won't actually verify this for offline login
-				challenge: window.crypto.getRandomValues(new Uint8Array(32)),
-			});
+			// Throwaway challenge, we won't actually verify this for offline login
+			const getOptions = makeGetOptions(window.crypto.getRandomValues(new Uint8Array(32)));
 			return {
 				getOptions: getOptions
 			}
