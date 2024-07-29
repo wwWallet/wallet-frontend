2ddbd00ee8725d9be3d880d1b915f6ef739cd3a7
Emil Lundberg <emil@yubico.com>
2024-07-29 12:29:56 +0200

    Sort keys in firebase config

--- src/config.ts
+++ src/config.ts
@@ -4,12 +4,12 @@ export const DID_KEY_VERSION = process.env.REACT_APP_DID_KEY_VERSION;
 export const DISPLAY_CONSOLE = process.env.REACT_APP_DISPLAY_CONSOLE;
 export const FIREBASE = {
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
 	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_FIREBASE_APP_ID,
-	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 export const FIREBASE_VAPIDKEY = process.env.REACT_APP_FIREBASE_VAPIDKEY;
 export const LOGIN_WITH_PASSWORD: boolean = process.env.REACT_APP_LOGIN_WITH_PASSWORD === 'true';
