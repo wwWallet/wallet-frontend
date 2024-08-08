a74fc2c5ae4835168f74e310b71d15ba306b332e
Emil Lundberg <emil@yubico.com>
2024-08-09 01:03:18 +0200

    Fix prop names in History.js
    Fixes these lints that appear when opening details of a credential:
    
    ```
    Warning: Invalid DOM property `stroke-linecap`. Did you mean `strokeLinecap`?
    Warning: Invalid DOM property `stroke-linejoin`. Did you mean `strokeLinejoin`?
    Warning: Invalid DOM property `stroke-width`. Did you mean `strokeWidth`?
    Warning: Invalid DOM property `class`. Did you mean `className`?
    ```

--- src/pages/History/History.js
+++ src/pages/History/History.js
@@ -120,13 +120,13 @@ const History = () => {
 					overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
 				>
 					{/* Popup content */}
-					<div class="flex items-start justify-between mb-2 dark:border-gray-600">
+					<div className="flex items-start justify-between mb-2 dark:border-gray-600">
 						<h2 className="right text-lg font-bold text-primary dark:text-white">
 							{t('pageHistory.popupTitle')}
 						</h2>
-						<button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" onClick={() => { setImageModalOpen(false); setCurrentSlide(1); }}>
-							<svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-								<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+						<button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" onClick={() => { setImageModalOpen(false); setCurrentSlide(1); }}>
+							<svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+								<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
 							</svg>
 						</button>
 					</div>
