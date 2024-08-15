72fc5df967d224194d79a08ef9e2eef8925f538c
Emil Lundberg <emil@yubico.com>
2024-08-15 18:17:59 +0200

    Delete unnecessary state needPrfRetry

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -208,7 +208,6 @@ const WebauthnSignupLogin = ({
 	const [inProgress, setInProgress] = useState(false);
 	const [name, setName] = useState("");
 	const [error, setError] = useState<React.ReactNode>('');
-	const [needPrfRetry, setNeedPrfRetry] = useState(false);
 	const [resolvePrfRetryPrompt, setResolvePrfRetryPrompt] = useState<(accept: boolean) => void>(null);
 	const [prfRetryAccepted, setPrfRetryAccepted] = useState(false);
 	const navigate = useNavigate();
@@ -228,11 +227,9 @@ const WebauthnSignupLogin = ({
 	);
 
 	const promptForPrfRetry = async (): Promise<boolean> => {
-		setNeedPrfRetry(true);
 		return new Promise((resolve: (accept: boolean) => void, reject) => {
 			setResolvePrfRetryPrompt(() => resolve);
 		}).finally(() => {
-			setNeedPrfRetry(false);
 			setPrfRetryAccepted(true);
 			setResolvePrfRetryPrompt(null);
 		});
@@ -360,7 +357,6 @@ const WebauthnSignupLogin = ({
 	const onCancel = () => {
 		console.log("onCancel");
 		setInProgress(false);
-		setNeedPrfRetry(false);
 		setPrfRetryAccepted(false);
 		setResolvePrfRetryPrompt(null);
 		setIsSubmitting(false);
@@ -376,7 +372,7 @@ const WebauthnSignupLogin = ({
 		<form onSubmit={onSubmit}>
 			{inProgress || retrySignupFrom
 				? (
-					needPrfRetry
+					resolvePrfRetryPrompt
 						? (
 							<div className="text-center">
 								{
