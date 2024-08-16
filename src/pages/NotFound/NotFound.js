2634e7a1b7f1b617520d17932ebb756b4c6c5ba6
Emil Lundberg <emil@yubico.com>
2024-08-16 17:48:46 +0200

    Move <FadeInContentTransition> use to router level

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import logo from '../../assets/images/logo.png';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import GetButton from '../../components/Buttons/GetButton';
-import FadeInContentTransition from '../../components/FadeInContentTransition';
+
 
 const NotFound = () => {
 	const navigate = useNavigate();
@@ -15,33 +15,31 @@ const NotFound = () => {
 
 	return (
 		<section className="bg-gray-100 dark:bg-gray-900">
-			<FadeInContentTransition>
-				<div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen pb-20">
-					<a href="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-						<img className="w-20" src={logo} alt="logo" />
-					</a>
-					<h1 className="text-xl mb-7 font-bold leading-tight tracking-tight text-gray-900 md:text-2xl text-center dark:text-white">
-						{t('common.walletName')}
-					</h1>
-					<div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-						<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-							<h1 className="text-2xl font-bold leading-tight tracking-tight text-gray-900 md:text-4xl text-center dark:text-white">
-								{t('notFound.title')}
-							</h1>
+			<div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen pb-20">
+				<a href="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+					<img className="w-20" src={logo} alt="logo" />
+				</a>
+				<h1 className="text-xl mb-7 font-bold leading-tight tracking-tight text-gray-900 md:text-2xl text-center dark:text-white">
+					{t('common.walletName')}
+				</h1>
+				<div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+					<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+						<h1 className="text-2xl font-bold leading-tight tracking-tight text-gray-900 md:text-4xl text-center dark:text-white">
+							{t('notFound.title')}
+						</h1>
 
-							<p className='text-center'>
-								{t('notFound.message')}
-							</p>
-							<GetButton
-								content={t('notFound.homeButton')}
-								onClick={handleBackToHome}
-								variant="secondary"
-								additionalClassName='w-full'
-							/>
-						</div>
+						<p className='text-center'>
+							{t('notFound.message')}
+						</p>
+						<GetButton
+							content={t('notFound.homeButton')}
+							onClick={handleBackToHome}
+							variant="secondary"
+							additionalClassName='w-full'
+						/>
 					</div>
 				</div>
-			</FadeInContentTransition>
+			</div>
 		</section>
 	);
 };
