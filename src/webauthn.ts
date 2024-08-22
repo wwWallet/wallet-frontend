251e57ba93a411507813941166e6f021effca634
Emil Lundberg <emil@yubico.com>
2024-08-22 17:31:41 +0200

    Prepare for using sign extension

--- src/webauthn.ts
+++ src/webauthn.ts
@@ -1,5 +1,45 @@
 import * as cbor from 'cbor-web';
 
+
+export type ParsedCOSEKey = {
+	kty: number | string,
+	kid?: Uint8Array,
+	alg?: COSEAlgorithmIdentifier,
+	[name: string]: any,
+};
+
+export type ParsedCOSEKeyEc2Public = ParsedCOSEKey & {
+	kty: 2,
+	kid?: Uint8Array,
+	alg?: COSEAlgorithmIdentifier,
+	crv: number,
+	x: Uint8Array,
+	y: Uint8Array,
+};
+
+export type ParsedCOSEKeyArkgPubSeed = ParsedCOSEKey & {
+	kty: -65537,
+	pkBl: ParsedCOSEKey,
+	pkKem: ParsedCOSEKey,
+};
+
+export type ParsedCOSEKeyRef = {
+	kty: number | string,
+	kid: Uint8Array,
+	alg?: COSEAlgorithmIdentifier,
+	[name: string]: any,
+};
+
+export type ParsedCOSEKeyRefArkgDerivedBase = ParsedCOSEKeyRef & {
+	kty: -65538,
+	kh: Uint8Array,
+};
+
+export type ParsedCOSEKeyRefArkgDerived = ParsedCOSEKeyRefArkgDerivedBase & {
+	info: Uint8Array,
+}
+
+
 export function parseAuthenticatorData(bytes: Uint8Array): {
 	rpIdHash: Uint8Array,
 	flags: {
@@ -61,7 +101,10 @@ export function parseAuthenticatorData(bytes: Uint8Array): {
 	}
 }
 
-function parseAttestedCredentialData(bytes: Uint8Array): [{ aaguid: Uint8Array, credentialId: Uint8Array, credentialPublicKey: { [key: number]: any } }, { [extensionId: string]: any }?] {
+function parseAttestedCredentialData(bytes: Uint8Array): [
+	{ aaguid: Uint8Array, credentialId: Uint8Array, credentialPublicKey: { [key: number]: any } },
+	{ [extensionId: string]: any }?
+] {
 	const aaguid = bytes.slice(0, 16);
 	const credentialIdLength = new DataView(bytes.buffer).getUint16(16, false);
 	const credentialId = bytes.slice(16 + 2, 16 + 2 + credentialIdLength);
@@ -76,8 +119,25 @@ function parseAttestedCredentialData(bytes: Uint8Array): [{ aaguid: Uint8Array, 
 	];
 }
 
+export function getAuthenticatorExtensionOutputs(credential: PublicKeyCredential): { [extensionId: string]: any } {
+	const authenticatorData = (
+		"authenticatorData" in credential.response
+			? credential.response.authenticatorData
+			: ("attestationObject" in credential.response
+				? cbor.decodeFirstSync(credential.response.attestationObject)["authData"]
+				: null
+			)
+	);
+	if (authenticatorData === null) {
+		throw new Error(`Failed to get authenticator data from credential: ${credential}`, { cause: { credential } });
+	}
+
+	return parseAuthenticatorData(authenticatorData).extensions;
+}
+
 export async function importCosePublicKey(cose: cbor.Map): Promise<CryptoKey> {
 	const coseKey = parseCoseKeyEc2Public(cose);
+	const [algorithm, keyUsages] = getEcKeyImportParams(coseKey);
 	const rawBytes = new Uint8Array([
 		0x04,
 		...new Uint8Array(Math.max(0, 32 - coseKey.x.length)),
@@ -85,13 +145,32 @@ export async function importCosePublicKey(cose: cbor.Map): Promise<CryptoKey> {
 		...new Uint8Array(Math.max(0, 32 - coseKey.y.length)),
 		...coseKey.y,
 	]);
-	return await crypto.subtle.importKey(
-		"raw",
-		rawBytes,
-		{ name: "ECDSA", namedCurve: "P-256" },
-		true,
-		["verify"],
-	);
+	return await crypto.subtle.importKey("raw", rawBytes, algorithm, true, keyUsages);
+}
+
+function getEcKeyImportParams(cose: ParsedCOSEKeyEc2Public): [EcKeyImportParams, KeyUsage[]] {
+	const { alg, crv } = cose;
+	switch (alg) {
+		case -7: // ES256
+			switch (crv) {
+				case 1: // P-256
+					return [{ name: "ECDSA", namedCurve: "P-256" }, ["verify"]];
+				default:
+					throw new Error(`Unsupported COSE elliptic curve: ${crv}`, { cause: { crv } })
+			}
+
+		case -25: // ECDH-ES + HKDF-256
+			switch (crv) {
+				case 1: // P-256
+					return [{ name: "ECDH", namedCurve: "P-256" }, ["deriveBits", "deriveKey"]];
+
+				default:
+					throw new Error(`Unsupported COSE elliptic curve: ${crv}`, { cause: { crv } })
+			}
+
+		default:
+			throw new Error(`Unsupported COSE algorithm: ${alg}`, { cause: { alg } })
+	}
 }
 
 export function parseCoseKeyEc2Public(cose: cbor.Map): ParsedCOSEKeyEc2Public {
@@ -103,6 +182,7 @@ export function parseCoseKeyEc2Public(cose: cbor.Map): ParsedCOSEKeyEc2Public {
 			switch (alg) {
 
 				case -7: // ES256
+				case -25: // ECDH-ES + HKDF-256
 					const crv = cose.get(-1);
 					switch (crv) {
 
@@ -124,7 +204,7 @@ export function parseCoseKeyEc2Public(cose: cbor.Map): ParsedCOSEKeyEc2Public {
 								}
 								return { kty, alg, crv, x, y };
 							} else {
-								throw new Error(`Invalid COSE EC2 ES256 key: missing x or y`, { cause: { x, y } });
+								throw new Error(`Invalid COSE EC2 ES256 or ECDH key: missing x or y`, { cause: { x, y } });
 							}
 
 						default:
@@ -137,5 +217,46 @@ export function parseCoseKeyEc2Public(cose: cbor.Map): ParsedCOSEKeyEc2Public {
 
 		default:
 			throw new Error(`Unsupported COSE key type: ${kty}`, { cause: { kty } });
+	}
+}
+
+export function parseCoseKeyArkgPubSeed(cose: cbor.Map): ParsedCOSEKeyArkgPubSeed {
+	const kty = cose.get(1);
+	switch (kty) {
+
+		case -65537: // ARKG-pub-seed https://yubico.github.io/arkg-rfc/draft-bradleylundberg-cfrg-arkg.html#name-cose-key-types-registration
+			const pkBl = parseCoseKeyEc2Public(cose.get(-1));
+			const pkKem = parseCoseKeyEc2Public(cose.get(-2));
+			return { kty, pkBl, pkKem };
+
+		default:
+			throw new Error(`Unsupported COSE key type: ${kty}`, { cause: { kty } });
+	}
+}
+
+export function parseCoseRefArkgDerivedBase(cose: cbor.Map): ParsedCOSEKeyRefArkgDerivedBase {
+	const kid = cose.get(2);
+	if (!(kid instanceof Uint8Array)) {
+		throw new Error(
+			`Incorrect type of "kid (2)" attribute of ARKG-derived COSE_Key_Ref: ${typeof kid} ${kid}`,
+			{ cause: { kid } },
+		);
+	}
+
+	const kty = cose.get(1);
+	switch (kty) {
+
+		case -65538: // ARKG-derived https://yubico.github.io/arkg-rfc/draft-bradleylundberg-cfrg-arkg.html#name-cose-key-types-registration
+			const kh = cose.get(-1);
+			if (!(kh instanceof Uint8Array)) {
+				throw new Error(
+					`Incorrect type of "kh (-1)" attribute of ARKG-derived COSE_Key_Ref: ${typeof kh} ${kh}`,
+					{ cause: { kh } },
+				);
+			}
+			return { kty, kid, kh };
+
+		default:
+			throw new Error(`Unsupported COSE_Key_Ref type: ${kty}`, { cause: { kty } });
 	}
 }
