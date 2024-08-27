75901d5c69344247eb42df53b23d3dc3af8992b4
gkatrakazas <grigoriskatrakazas@gmail.com>
2024-08-27 10:36:01 +0300

    Refactor CredentialsContext: Add polling for new VCs when no notification, improve recognition new VC logic, streamline code

--- src/context/CredentialsContext.js
+++ src/context/CredentialsContext.js
@@ -1,7 +1,8 @@
-import React, { createContext, useState, useCallback, useContext } from 'react';
+import React, { createContext, useState, useCallback, useContext, useRef } from 'react';
 import { useApi } from '../api';
 import { extractCredentialFriendlyName } from '../functions/extractCredentialFriendlyName';
 import OnlineStatusContext from '../context/OnlineStatusContext';
+import { getItem } from '../indexedDB';
 
 const CredentialsContext = createContext();
 
@@ -10,30 +11,96 @@ export const CredentialsProvider = ({ children }) => {
 	const api = useApi(isOnline);
 	const [vcEntityList, setVcEntityList] = useState([]);
 	const [latestCredentials, setLatestCredentials] = useState(new Set());
+	const intervalId = useRef(null);
+	const isPolling = useRef(false);
+
+	const fetchVcData = async () => {
+		const response = await api.get('/storage/vc');
+		const fetchedVcList = response.data.vc_list;
+
+		const vcEntityList = await Promise.all(fetchedVcList.map(async vcEntity => {
+			const name = await extractCredentialFriendlyName(vcEntity.credential);
+			return { ...vcEntity, friendlyName: name };
+		}));
+
+		vcEntityList.sort((vcA, vcB) => new Date(vcB.issuanceDate) - new Date(vcA.issuanceDate));
+
+		return vcEntityList;
+	};
+
+	const updateVcListAndLatestCredentials = (vcEntityList) => {
+		setLatestCredentials(new Set(vcEntityList.filter(vc => vc.issuanceDate === vcEntityList[0].issuanceDate).map(vc => vc.id)));
+
+		setTimeout(() => {
+			setLatestCredentials(new Set());
+		}, 2000);
+
+		setVcEntityList(vcEntityList);
+	};
+
+	const pollForCredentials = () => {
+		let attempts = 0;
+		isPolling.current = true;
+
+		intervalId.current = setInterval(async () => {
+			const urlParams = new URLSearchParams(window.location.search);
+			if (!urlParams.has('code')) {
+				console.log('Code parameter no longer exists, stopping polling');
+				isPolling.current = false;
+				clearInterval(intervalId.current);
+				return;
+			}
+
+			if (!isPolling.current) {
+				clearInterval(intervalId.current);
+				return;
+			}
+
+			attempts += 1;
+			const sessionState = JSON.parse(sessionStorage.getItem('sessionState'));
+			const userId = sessionState.uuid;
+			const previousVcList = await getItem("vc", userId);
+			const previousSize = previousVcList.vc_list.length;
+
+			const vcEntityList = await fetchVcData();
+
+			if (previousSize < vcEntityList.length) {
+				console.log('Found new credentials, stopping polling');
+				isPolling.current = false;
+				clearInterval(intervalId.current);
+				updateVcListAndLatestCredentials(vcEntityList);
+			}
+
+			if (attempts >= 10) {
+				console.log('Max attempts reached, stopping polling');
+				isPolling.current = false;
+				clearInterval(intervalId.current);
+			}
+		}, 1000);
+	};
 
 	const getData = useCallback(async () => {
 		try {
-			const response = await api.get('/storage/vc');
-			const fetchedVcList = response.data.vc_list;
-			const vcEntityList = await Promise.all(fetchedVcList.map(async vcEntity => {
-				const name = await extractCredentialFriendlyName(vcEntity.credential);
-				return { ...vcEntity, friendlyName: name };
-			}));
-			vcEntityList.sort((vcA, vcB) => new Date(vcB.issuanceDate) - new Date(vcA.issuanceDate));
+			const sessionState = JSON.parse(sessionStorage.getItem('sessionState'));
+			const userId = sessionState.uuid;
+			const previousVcList = await getItem("vc", userId);
+			const previousSize = previousVcList.vc_list.length;
+			const vcEntityList = await fetchVcData();
+			setVcEntityList(vcEntityList);
 
-			const latestIssuanceDate = vcEntityList[0]?.issuanceDate;
-			const latestCreds = new Set(vcEntityList.filter(vc => vc.issuanceDate === latestIssuanceDate).map(vc => vc.id));
+			const shouldPoll = window.location.search.includes('code') && sessionStorage.getItem('tokenSentInSession') === 'false';
+			const newCredentialsFound = previousSize < vcEntityList.length;
 
-			if (window.location.search.includes('code')) {
-				setLatestCredentials(latestCreds);
-				setTimeout(() => {
-					setLatestCredentials(new Set());
-				}, 2000);
+			if (shouldPoll && !newCredentialsFound) {
+				console.log("No new credentials, starting polling");
+				pollForCredentials();
+			} else if (newCredentialsFound) {
+				console.log("Found new credentials, no need to poll");
+				updateVcListAndLatestCredentials(vcEntityList);
 			} else {
+				setVcEntityList(vcEntityList);
 				setLatestCredentials(new Set());
 			}
-
-			setVcEntityList(vcEntityList);
 		} catch (error) {
 			console.error('Failed to fetch data', error);
 		}
