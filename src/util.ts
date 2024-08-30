2d1bebd26b987537989ff05ffdeeba3e120259e6
Emil Lundberg <emil@yubico.com>
2024-08-30 13:39:16 +0200

    Extract common functions to util module

--- src/util.ts
+++ src/util.ts
@@ -1,9 +1,39 @@
-function toU8(b: BufferSource) {
-	if (b instanceof ArrayBuffer) {
+export function toU8(b: BufferSource): Uint8Array {
+	if (b instanceof Uint8Array) {
+		return b;
+	} else if (b instanceof ArrayBuffer) {
 		return new Uint8Array(b);
 	} else {
 		return new Uint8Array(b.buffer);
 	}
+}
+
+export function concat(...b: BufferSource[]): ArrayBuffer {
+	return b.map(toU8).reduce((a, b) => new Uint8Array([...a, ...b]), new Uint8Array([])).buffer;
+}
+
+/**
+	Convert a big-endian octet string to a nonnegative integer.
+
+	@see https://www.rfc-editor.org/rfc/rfc8017.html#section-4.2
+	*/
+export function OS2IP(binary: BufferSource): bigint {
+	return toU8(binary).reduce(
+		(result: bigint, b: number) => (result << 8n) + BigInt(b),
+		0n,
+	);
+}
+
+/**
+	Convert a nonnegative integer to a big-endian octet string of a specified length.
+
+	@see https://www.rfc-editor.org/rfc/rfc8017.html#section-4.1
+	*/
+export function I2OSP(a: bigint, length: number): ArrayBuffer {
+	return new Uint8Array(length).map(
+		(_, i: number): number =>
+			Number(BigInt.asUintN(8, a >> (BigInt(length - 1 - i) * 8n)))
+	).buffer;
 }
 
 export function toBase64(binary: BufferSource): string {
