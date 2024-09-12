41ef0d4f1067c6e519251c02c33f5ff4b31a80fb
Emil Lundberg <emil@yubico.com>
2024-09-12 19:50:49 +0200

    Extract <Button variant="outline">

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -6,6 +6,7 @@ export type Variant = (
 	| 'tertiary'
 	| 'cancel'
 	| 'delete'
+	| 'outline'
 	| 'link'
 	| 'custom'
 );
@@ -45,6 +46,8 @@ const Button = ({
 				return `${commonClasses} ${!disabled ? "text-gray-900 bg-gray-300 hover:bg-gray-400" : "text-white bg-gray-300 cursor-not-allowed hover:bg-gray-300"}`;
 			case 'delete':
 				return `${commonClasses} ${!disabled ? "text-white bg-red-600 hover:bg-red-700" : "text-red-400 bg-gray-300 hover:bg-gray-300 cursor-not-allowed"}`;
+			case 'outline':
+				return `bg-white px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-md hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-white ${!disabled ? 'cursor-pointer' : 'text-gray-300 border-gray-300 dark:text-gray-700 dark:border-gray-700 cursor-not-allowed' }`;
 			case 'link':
 				return `font-medium ${!disabled ? "text-primary dark:text-primary-light hover:underline" : "text-gray-400 cursor-not-allowed"}`;
 			default:
