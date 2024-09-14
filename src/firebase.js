71fb1a2ad451d379e9beb1bbe6b916aa1799ccae
gkatrakazas <grigoriskatrakazas@gmail.com>
2024-09-14 11:03:41 +0300

    Fix the init of firebase service worker

--- src/firebase.js
+++ src/firebase.js
@@ -5,26 +5,14 @@ import * as config from './config';
 
 
 let messaging = null;
-let supported = false;
 
 export const notificationApiIsSupported = () =>
 	'Notification' in window &&
 	'serviceWorker' in navigator &&
 	'PushManager' in window
 
-const initializeFirebase = async () => {
-	if (notificationApiIsSupported()) {
-		supported = await isSupported();
-		if (supported) {
-			initializeApp(config.FIREBASE);
-			messaging = getMessaging();
-		}
-	}
-	console.log("Supported", supported);
-};
-
 export async function register() {
-	if (supported && 'serviceWorker' in navigator) {
+	if (isSupported() && 'serviceWorker' in navigator) {
 		try {
 			const registration = await navigator.serviceWorker.register('/firebase-messaging-sw.js', { scope: '/notifications/' });
 			console.log('App: Firebase Messaging Service Worker registered! Scope is:', registration.scope);
@@ -37,7 +25,7 @@ export async function register() {
 };
 
 const requestForToken = async () => {
-	if (!supported) {
+	if (!isSupported()) {
 		return null;
 	}
 	if (messaging) {
@@ -71,7 +59,7 @@ const requestForToken = async () => {
 
 
 const reRegisterServiceWorkerAndGetToken = async () => {
-	if (!supported) {
+	if (!isSupported()) {
 		return null;
 	}
 	if ('serviceWorker' in navigator) {
@@ -100,7 +88,7 @@ const reRegisterServiceWorkerAndGetToken = async () => {
 };
 
 export const fetchToken = async () => {
-	if (supported && messaging) {
+	if (isSupported() && messaging) {
 		const token = await requestForToken();
 		console.log('token:', token);
 		if (token) {
@@ -123,40 +111,27 @@ export const fetchToken = async () => {
 
 export const onMessageListener = () =>
 	new Promise((resolve) => {
-		if (supported) {
+		if (isSupported()) {
 			onMessage(messaging, (payload) => {
 				resolve(payload);
 			});
 		}
 	});
 
-const initializeMessaging = async () => {
-	// Check for service worker
-	if (supported && 'serviceWorker' in navigator) {
-		try {
-			const registration = await navigator.serviceWorker.getRegistration('/firebase-messaging-sw.js');
-			if (registration) {
-				console.log('Service Worker registered', registration);
-				return getMessaging();
-			} else {
-				console.log('Service Worker registration failed');
-			}
-		} catch (error) {
-			console.error('Service Worker registration failed with', error);
-		}
-	} else {
-		console.log('Service Workers are not supported in this browser.');
-	}
-};
 
 const initializeFirebaseAndMessaging = async () => {
-	await initializeFirebase();
-	const messaging = await initializeMessaging();
-	// Now you can use the messaging object if it's available
-	if (messaging) {
-		// Do something with the messaging object
-	} else {
-		console.log('Messaging is not initialized.');
+	if (notificationApiIsSupported()) {
+		let supported = await isSupported();
+		console.log("Supported", supported);
+		if (supported) {
+			initializeApp(config.FIREBASE);
+			messaging = getMessaging();
+			if (messaging) {
+				console.log('Messaging is initialized.');
+			} else {
+				console.log('Messaging is not initialized.');
+			}
+		}
 	}
 };
 
