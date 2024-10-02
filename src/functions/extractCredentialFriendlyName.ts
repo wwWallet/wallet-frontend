2d254f94c12abcf650a9fe3ce17544bb657a8e6a
kkmanos <koukoularisman@gmail.com>
2024-10-02 12:58:40 +0300

    simplified parsing

--- src/functions/extractCredentialFriendlyName.ts
+++
@@ -1,7 +0,0 @@
-import { parseCredential } from "./parseCredential";
-
-
-export const extractCredentialFriendlyName = async (credential: string | object): Promise<string | undefined> => {
-	const parsedCredential = await parseCredential(credential) as any;
-	return parsedCredential.name ?? parsedCredential.id;
-}
--- src/functions/extractCredentialImage.ts
+++
@@ -1,6 +0,0 @@
-import { parseCredential } from "./parseCredential"
-
-export const extractCredentialImageURL = async (credential: string | object): Promise<string | undefined> => {
-	const parsedCredential = await parseCredential(credential) as any;
-	return parsedCredential?.credentialBranding?.image?.url;
-}
