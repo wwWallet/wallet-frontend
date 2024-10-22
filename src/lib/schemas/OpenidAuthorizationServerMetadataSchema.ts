0559b24c67f4a3bd8c8628ff092b5fc53b90b64d
kkmanos <koukoularisman@gmail.com>
2024-10-22 10:21:49 +0300

    schema metadata validation

--- src/lib/schemas/OpenidAuthorizationServerMetadataSchema.ts
+++ src/lib/schemas/OpenidAuthorizationServerMetadataSchema.ts
@@ -5,11 +5,11 @@ export const OpenidAuthorizationServerMetadataSchema = z.object({
 	authorization_endpoint: z.string(),
 	token_endpoint: z.string(),
 	pushed_authorization_request_endpoint: z.string(),
-	require_pushed_authorization_requests: z.boolean(),
+	require_pushed_authorization_requests: z.boolean().optional(),
 	token_endpoint_auth_methods_supported: z.array(z.string()),
 	response_types_supported: z.array(z.string()),
 	code_challenge_methods_supported: z.array(z.string()),
-	dpop_signing_alg_values_supported: z.array(z.string()),
+	dpop_signing_alg_values_supported: z.array(z.string()).optional(),
 });
 
 
