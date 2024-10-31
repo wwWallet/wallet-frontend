8e74e21ae9a7ea261c0c232346b2feb22cc8fa11
kkmanos <koukoularisman@gmail.com>
2024-10-31 12:58:07 +0200

    fix on multiple credential configuration selection

--- src/components/Popups/RedirectPopup.js
+++ src/components/Popups/RedirectPopup.js
@@ -23,7 +23,7 @@ const RedirectPopup = ({ loading, availableCredentialConfigurations, onClose, ha
 
 	const handleOptionChange = (event) => {
 		if (availableCredentialConfigurations) {
-			setSelectedConfiguration(availableCredentialConfigurations[event.target.value]);
+			setSelectedConfiguration(event.target.value);
 		}
 	};
 
@@ -48,7 +48,7 @@ const RedirectPopup = ({ loading, availableCredentialConfigurations, onClose, ha
 							value={credentialConfigurationId}
 							name="default-radio"
 							class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-							checked={selectedConfiguration === availableCredentialConfigurations[credentialConfigurationId]}
+							checked={selectedConfiguration === credentialConfigurationId}
 							aria-label={`Option ${credentialConfigurationId}`}
 						/>
 						<label for={"radio-" + index} class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
