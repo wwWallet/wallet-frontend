0ee6e9f8c8afb418b3c07b61009eb6824d7d5ad2
gkatrakazas <grigoriskatrakazas@gmail.com>
2025-01-18 17:40:44 +0200

    On useNotificationListener use a ref for getData to prevent unnecessary re-renders

--- src/hooks/useNotificationListener.js
+++ src/hooks/useNotificationListener.js
@@ -1,10 +1,17 @@
-import { useEffect, useState, useContext } from 'react';
+import { useEffect, useState, useContext, useRef } from 'react';
 import { onMessageListener } from '../firebase';
 import CredentialsContext from '../context/CredentialsContext';
 
 const useNotificationListener = () => {
 	const [notification, setNotification] = useState(null);
 	const { getData } = useContext(CredentialsContext);
+
+	// Use a ref to store getData to prevent triggering useEffect when it changes
+	const getDataRef = useRef(getData);
+
+	useEffect(() => {
+		getDataRef.current = getData; // Keep the ref updated with the latest getData function
+	}, [getData]);
 
 	useEffect(() => {
 		const listenForNotifications = () => {
@@ -14,15 +21,14 @@ const useNotificationListener = () => {
 						title: payload?.notification?.title,
 						body: payload?.notification?.body,
 					})
-					getData();
+					getDataRef.current();
 				})
 				.catch((err) => {
 					console.error('Failed to receive notification:', err);
 				});
 		};
-
 		listenForNotifications();
-	}, [getData]);
+	}, []);
 
 	return notification;
 };
