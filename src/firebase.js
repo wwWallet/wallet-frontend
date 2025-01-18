389612539bc92f6ed818d2b4f59087e0a9953d5e
gkatrakazas <grigoriskatrakazas@gmail.com>
2025-01-18 15:38:22 +0200

    Rename firebase service worker scope

--- src/firebase.js
+++ src/firebase.js
@@ -16,11 +16,11 @@ export async function isEnabledAndIsSupported() {
 export async function register() {
 	if (await isEnabledAndIsSupported() && 'serviceWorker' in navigator) {
 		try {
-			const existingRegistration = await navigator.serviceWorker.getRegistration('/notifications/');
+			const existingRegistration = await navigator.serviceWorker.getRegistration('/firebase-cloud-messaging-push-scope');
 			if (existingRegistration) {
 				console.log('Service Worker is already registered. Scope:', existingRegistration.scope);
 			} else {
-				const registration = await navigator.serviceWorker.register('/firebase-messaging-sw.js', { scope: '/notifications/' });
+				const registration = await navigator.serviceWorker.register('/firebase-messaging-sw.js', { scope: '/firebase-cloud-messaging-push-scope' });
 				console.log('App: Firebase Messaging Service Worker registered! Scope is:', registration.scope);
 			}
 		} catch (err) {
@@ -70,7 +70,7 @@ const reRegisterServiceWorkerAndGetToken = async () => {
 	if ('serviceWorker' in navigator) {
 		try {
 			// Re-register the service worker
-			const registration = await navigator.serviceWorker.register('/firebase-messaging-sw.js', { scope: '/notifications/' });
+			const registration = await navigator.serviceWorker.register('/firebase-messaging-sw.js', { scope: '/firebase-cloud-messaging-push-scope' });
 			if (registration) {
 				console.log('Service Worker re-registered', registration);
 				const token = await requestForToken();
