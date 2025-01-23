fd4b8eede6ee2b49b3b8b2bb1c4b629b70bcab5c
gkatrakazas <grigoriskatrakazas@gmail.com>
2025-01-23 14:32:29 +0200

    Remove unnecessary console logs

--- src/components/Notifications/BackgroundNotificationClickHandler.js
+++ src/components/Notifications/BackgroundNotificationClickHandler.js
@@ -3,11 +3,8 @@ import { useEffect } from 'react';
 const BackgroundNotificationClickHandler = () => {
 	useEffect(() => {
 		const handleNotificationClickMessage = (event) => {
-			console.log('Message received from service worker:', event);
-
 			if (event.data?.type === 'navigate') {
 				const targetUrl = event.data.url || '/';
-				console.log(`Redirecting to: ${targetUrl}`);
 				window.location.href = targetUrl; // Navigate to the target URL
 			}
 		};
