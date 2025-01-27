004fcbf8773e28ffe6e3d1be5e6cdcb8ae81a0df
gkatrakazas <grigoriskatrakazas@gmail.com>
2025-01-27 14:30:32 +0200

    Refactor UriHandler: Split logic into smaller functions

--- src/UriHandler.tsx
+++ src/UriHandler.tsx
@@ -12,20 +12,17 @@ import OpenID4VPContext from "./context/OpenID4VPContext";
 const MessagePopup = React.lazy(() => import('./components/Popups/MessagePopup'));
 const PinInputPopup = React.lazy(() => import('./components/Popups/PinInput'));
 
-
-
 export const UriHandler = ({ children }) => {
-	const { updateOnlineStatus } = useContext(StatusContext);
 
-	const { isLoggedIn, keystore } = useContext(SessionContext);
 	const location = useLocation();
 	const [url, setUrl] = useState(window.location.href);
 
+	const { updateOnlineStatus } = useContext(StatusContext);
+	const { isLoggedIn, keystore } = useContext(SessionContext);
 	const { openID4VCI } = useContext(OpenID4VCIContext);
 	const { openID4VP } = useContext(OpenID4VPContext);
 
 	const [showPinInputPopup, setShowPinInputPopup] = useState<boolean>(false);
-
 	const [showMessagePopup, setMessagePopup] = useState<boolean>(false);
 	const [textMessagePopup, setTextMessagePopup] = useState<{ title: string, description: string }>({ title: "", description: "" });
 	const [typeMessagePopup, setTypeMessagePopup] = useState<string>("");
@@ -46,85 +43,125 @@ export const UriHandler = ({ children }) => {
 		async function handle(urlToCheck: string) {
 
 			const u = new URL(urlToCheck);
-			if (u.searchParams.size === 0) {
-				return;
-			}
+			if (u.searchParams.size === 0) return;
 
-			const userHandleB64u = keystore.getUserHandleB64u();
-			if (!userHandleB64u) {
-				return;
-			}
+			if (!keystore.getUserHandleB64u()) return;
 
 			if (u.protocol === 'openid-credential-offer' || u.searchParams.get('credential_offer') || u.searchParams.get('credential_offer_uri')) {
-				openID4VCI.handleCredentialOffer(u.toString()).then(({ credentialIssuer, selectedCredentialConfigurationId, issuer_state }) => {
-					console.log("Generating authorization request...");
-					return openID4VCI.generateAuthorizationRequest(credentialIssuer, selectedCredentialConfigurationId, issuer_state);
-				}).then((res) => {
-					if ('url' in res && res.url) {
-						window.location.href = res.url;
-					}
-				})
-					.catch((err) => console.error(err));
-				return;
+				console.log("Handling Credential Offer ...");
+				await handleCredentialOffer(u.toString(), openID4VCI);
 			}
 			else if (u.searchParams.get('code')) {
 				console.log("Handling authorization response...");
-				addLoader();
-				openID4VCI.handleAuthorizationResponse(u.toString()).then(() => {
-					removeLoader();
-				}).catch(err => {
-					console.log("Error during the handling of authorization response")
-					window.history.replaceState({}, '', `${window.location.pathname}`);
-					console.error(err)
-					removeLoader();
-				})
+				await handleAuthorizationResponse(u.toString(), openID4VCI, addLoader, removeLoader);
 			}
 			else if (u.searchParams.get('client_id') && u.searchParams.get('request_uri')) {
-				await openID4VP.handleAuthorizationRequest(u.toString()).then((result) => {
-					console.log("Result = ", result);
-					if ('err' in result) {
-						if (result.err === HandleAuthorizationRequestError.INSUFFICIENT_CREDENTIALS) {
-							setTextMessagePopup({ title: `${t('messagePopup.insufficientCredentials.title')}`, description: `${t('messagePopup.insufficientCredentials.description')}` });
-							setTypeMessagePopup('error');
-							setMessagePopup(true);
-						}
-						else if (result.err === HandleAuthorizationRequestError.NONTRUSTED_VERIFIER) {
-							setTextMessagePopup({ title: `${t('messagePopup.nonTrustedVerifier.title')}`, description: `${t('messagePopup.nonTrustedVerifier.description')}` });
-							setTypeMessagePopup('error');
-							setMessagePopup(true);
-						}
-						return;
-					}
-					const { conformantCredentialsMap, verifierDomainName } = result;
-					const jsonedMap = Object.fromEntries(conformantCredentialsMap);
-					console.log("Prompting for selection..")
-					return openID4VP.promptForCredentialSelection(jsonedMap, verifierDomainName);
-				}).then((selection) => {
-					if (!(selection instanceof Map)) {
-						return;
-					}
-					console.log("Selection = ", selection);
-					return openID4VP.sendAuthorizationResponse(selection);
-
-				}).then((res) => {
-					if (res && 'url' in res && res.url) {
-						window.location.href = res.url;
-					}
-				}).catch(err => {
-					console.log("Failed to handle authorization req");
-					console.error(err);
-				})
-				return;
+				console.log("Handling authorization Request...");
+				await handleAuthorizationRequest(u.toString(), openID4VP, t, setMessagePopup, setTextMessagePopup, setTypeMessagePopup);
 			}
 			else if (url && isLoggedIn && u.searchParams.get('state') && u.searchParams.get('error') && u.searchParams.get('error_description')) {
-				window.history.replaceState({}, '', `${window.location.pathname}`);
-				setTextMessagePopup({ title: u.searchParams.get('error'), description: u.searchParams.get('error_description') });
-				setTypeMessagePopup('error');
-				setMessagePopup(true);
+				console.log("Handling Error...");
+				handleError(u, setMessagePopup, setTextMessagePopup, setTypeMessagePopup);
 			}
 		}
 		handle(url);
 	}, [url, addLoader, removeLoader, t, keystore, isLoggedIn, openID4VCI, openID4VP]);
+
+	// handleCredentialOffer
+	const handleCredentialOffer = async (url: string, openID4VCI: any) => {
+		try {
+			const { credentialIssuer, selectedCredentialConfigurationId, issuer_state } = await openID4VCI.handleCredentialOffer(url);
+			const res = await openID4VCI.generateAuthorizationRequest(credentialIssuer, selectedCredentialConfigurationId, issuer_state);
+			if (res?.url) {
+				window.location.href = res.url;
+			}
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	// handleAuthorizationResponse
+	const handleAuthorizationResponse = async (url: string, openID4VCI: any, addLoader: () => void, removeLoader: () => void) => {
+		try {
+			addLoader();
+			await openID4VCI.handleAuthorizationResponse(url);
+		} catch (err) {
+			console.error("Error during the handling of authorization response", err);
+			window.history.replaceState({}, "", `${window.location.pathname}`);
+		} finally {
+			removeLoader();
+		}
+	};
+
+	// handleAuthorizationRequest
+	const handleAuthorizationRequest = async (
+		url: string,
+		openID4VP: any,
+		t: any,
+		setMessagePopup: React.Dispatch<React.SetStateAction<boolean>>,
+		setTextMessagePopup: React.Dispatch<React.SetStateAction<{ title: string; description: string }>>,
+		setTypeMessagePopup: React.Dispatch<React.SetStateAction<string>>
+	) => {
+		try {
+			const result = await openID4VP.handleAuthorizationRequest(url);
+			if (result?.err) {
+				handleAuthorizationError(result.err, t, setMessagePopup, setTextMessagePopup, setTypeMessagePopup);
+				return;
+			}
+			const { conformantCredentialsMap, verifierDomainName } = result;
+			const selection = await openID4VP.promptForCredentialSelection(
+				Object.fromEntries(conformantCredentialsMap),
+				verifierDomainName
+			);
+			if (selection instanceof Map) {
+				const res = await openID4VP.sendAuthorizationResponse(selection);
+				if (res?.url) window.location.href = res.url;
+			}
+		} catch (err) {
+			console.error("Failed to handle authorization request", err);
+		}
+	};
+
+	const handleAuthorizationError = (
+		error: HandleAuthorizationRequestError,
+		t: any,
+		setMessagePopup: React.Dispatch<React.SetStateAction<boolean>>,
+		setTextMessagePopup: React.Dispatch<React.SetStateAction<{ title: string; description: string }>>,
+		setTypeMessagePopup: React.Dispatch<React.SetStateAction<string>>
+	) => {
+		const messages = {
+			[HandleAuthorizationRequestError.INSUFFICIENT_CREDENTIALS]: {
+				title: t("messagePopup.insufficientCredentials.title"),
+				description: t("messagePopup.insufficientCredentials.description"),
+			},
+			[HandleAuthorizationRequestError.NONTRUSTED_VERIFIER]: {
+				title: t("messagePopup.nonTrustedVerifier.title"),
+				description: t("messagePopup.nonTrustedVerifier.description"),
+			},
+		};
+		const message = messages[error];
+		if (message) {
+			setTextMessagePopup(message);
+			setTypeMessagePopup("error");
+			setMessagePopup(true);
+		}
+	};
+
+	// handleError
+	const handleError = (
+		u: URL,
+		setMessagePopup: React.Dispatch<React.SetStateAction<boolean>>,
+		setTextMessagePopup: React.Dispatch<React.SetStateAction<{ title: string; description: string }>>,
+		setTypeMessagePopup: React.Dispatch<React.SetStateAction<string>>
+	) => {
+		window.history.replaceState({}, "", `${window.location.pathname}`);
+		setTextMessagePopup({
+			title: u.searchParams.get("error")!,
+			description: u.searchParams.get("error_description")!,
+		});
+		setTypeMessagePopup("error");
+		setMessagePopup(true);
+	};
 
 	return (
 		<>
@@ -138,7 +175,6 @@ export const UriHandler = ({ children }) => {
 		</>
 	);
 }
-
 
 export const withUriHandler: <P>(component: React.ComponentType<P>) => React.ComponentType<P> = (Component) =>
 	(props) => (
