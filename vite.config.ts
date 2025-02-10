9c478aec9a74d66a45051fab7dd89d124427de7e
gkatrakazas <grigoriskatrakazas@gmail.com>
2025-02-10 12:13:38 +0200

    setup react-app eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
 	base: '/',
 	plugins: [
 		react(),
 		svgr(),
+		eslint(),
 	],
 	resolve: {
 		alias: {
