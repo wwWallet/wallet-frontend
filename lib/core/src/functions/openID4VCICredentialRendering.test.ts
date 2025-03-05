bd7c6fd227328673c3c4bbd270343e5f8d85bdb6
kkmanos <koukoularisman@gmail.com>
2025-03-05 13:14:59 +0200

    defined interface for OpenID4VCICredentialRendering

---
+++ lib/core/src/functions/openID4VCICredentialRendering.test.ts
@@ -0,0 +1,11 @@
+import { assert, describe, it } from "vitest";
+import { OpenID4VCICredentialRendering } from "./openID4VCICredentialRendering";
+
+describe("The openID4VCICredentialRendering", () => {
+
+	it("can render credential in svg format", async () => {
+		const renderer = OpenID4VCICredentialRendering();
+
+
+	});
+})
---
+++ lib/core/src/functions/openID4VCICredentialRendering.ts
@@ -0,0 +1,9 @@
+import { OpenID4VCICredentialRendering } from "../interfaces";
+
+export function OpenID4VCICredentialRendering(): OpenID4VCICredentialRendering {
+	return {
+		renderCustomSvgTemplate(args) {
+			throw new Error("Not implemented");
+		},
+	}
+}
