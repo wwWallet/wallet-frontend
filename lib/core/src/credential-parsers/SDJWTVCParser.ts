427935842f47dae81492f985ca40c53e3e90834a
kkmanos <koukoularisman@gmail.com>
2025-03-11 18:47:12 +0200

    optimizations on lib core and AddCredentials comp

--- lib/core/src/credential-parsers/SDJWTVCParser.ts
+++ lib/core/src/credential-parsers/SDJWTVCParser.ts
@@ -3,7 +3,7 @@ import { CredentialParsingError } from "../error";
 import { Context, CredentialParser, HttpClient } from "../interfaces";
 import { VerifiableCredentialFormat } from "../types";
 import { CredentialRenderingService } from "../rendering";
-import { fromBase64 } from "../utils/util";
+import { getSdJwtVcMetadata } from "../utils/getSdJwtVcMetadata";
 
 
 
@@ -45,34 +45,6 @@ export function SDJWTVCParser(args: { context: Context, httpClient: HttpClient }
 
 	const cr = CredentialRenderingService();
 
-	async function getSdJwtVcMetadata(credential: string): Promise<{ credentialMetadata: any } | { error: "NOT_FOUND" }> {
-		try {
-			const credentialHeader = JSON.parse(new TextDecoder().decode(fromBase64(credential.split('.')[0] as string)));
-			const credentialPayload = JSON.parse(new TextDecoder().decode(fromBase64(credential.split('.')[1] as string)));
-
-			if (credentialHeader.vctm) {
-				const sdjwtvcMetadataDocument = credentialHeader.vctm.map((encodedMetadataDocument: string) =>
-					JSON.parse(new TextDecoder().decode(fromBase64(encodedMetadataDocument)))
-				).filter(((metadataDocument: Record<string, unknown>) => metadataDocument.vct === credentialPayload.vct))[0];
-				if (sdjwtvcMetadataDocument) {
-					return { credentialMetadata: sdjwtvcMetadataDocument };
-				}
-			}
-
-
-			// use vct to fetch metadata if hosted
-			const fetchResult = await args.httpClient.get(credentialPayload.vct);
-			if (fetchResult && fetchResult.status === 200 && (fetchResult.data as Record<string, unknown>).vct === credentialPayload.vct) {
-				return { credentialMetadata: fetchResult.data };
-			}
-
-			return { error: "NOT_FOUND" };
-		}
-		catch (err) {
-			console.log(err);
-			return { error: "NOT_FOUND" };
-		}
-	}
 
 	return {
 		async parse({ rawCredential }) {
@@ -115,7 +87,7 @@ export function SDJWTVCParser(args: { context: Context, httpClient: HttpClient }
 
 			let credentialFriendlyName: string | null = null;
 
-			const getSdJwtMetadataResult = await getSdJwtVcMetadata(rawCredential);
+			const getSdJwtMetadataResult = await getSdJwtVcMetadata(args.httpClient,rawCredential);
 			if (!('error' in getSdJwtMetadataResult)) {
 				const { credentialMetadata } = getSdJwtMetadataResult;
 
