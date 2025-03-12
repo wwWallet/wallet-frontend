72e33fd07fae0261d26f32a4a0aab3517f1c19d9
gkatrakazas <grigoriskatrakazas@gmail.com>
2025-03-12 08:25:37 +0200

    update CredentialInfo with dynamic display

--- src/components/Credentials/CredentialInfo.jsx
+++ src/components/Credentials/CredentialInfo.jsx
@@ -1,82 +1,78 @@
 import React from 'react';
-import { BiSolidCategoryAlt, BiSolidUserCircle } from 'react-icons/bi';
-import { AiFillCalendar } from 'react-icons/ai';
-import { RiPassExpiredFill } from 'react-icons/ri';
-import { MdTitle, MdGrade, MdOutlineNumbers } from 'react-icons/md';
-import { GiLevelEndFlag } from 'react-icons/gi';
 import { formatDate } from '../../functions/DateFormat';
-import useScreenType from '../../hooks/useScreenType';
+import { getLanguage } from '@/i18n';
+import { useTranslation } from 'react-i18next';
 
-const getFieldIcon = (fieldName) => {
-	switch (fieldName) {
-		case 'type':
-			return <BiSolidCategoryAlt size={25} className="inline mr-1" />;
-		case 'expdate':
-			return <RiPassExpiredFill size={25} className="inline mr-1" />;
-		case 'dateOfBirth':
-			return <AiFillCalendar size={25} className="inline mr-1" />;
-		case 'id':
-			return <MdOutlineNumbers size={25} className="inline mr-1" />;
-		case 'familyName':
-		case 'firstName':
-			return <BiSolidUserCircle size={25} className="inline mr-1" />;
-		case 'diplomaTitle':
-			return <MdTitle size={25} className="inline mr-1" />;
-		case 'eqfLevel':
-			return <GiLevelEndFlag size={25} className="inline mr-1" />;
-		case 'grade':
-			return <MdGrade size={25} className="inline mr-1" />;
-		default:
-			return null;
-	}
+const getLabelAndDescriptionByLang = (displayArray, lang, fallbackLang) => {
+	// Finding the display object based on language or fallback language
+	let displayObj = displayArray.find(d => getLanguage(d.lang) === lang) ||
+		displayArray.find(d => getLanguage(d.lang) === fallbackLang);
+	return {
+		label: displayObj ? displayObj.label : displayArray[0]?.label || '',
+		description: displayObj ? displayObj.description : displayArray[0]?.description || ''
+	};
 };
 
-const renderRow = (fieldName, label, fieldValue, screenType) => {
-
-	if (fieldValue) {
-		return (
-			<tr className="text-left">
-				<td className="font-bold text-primary dark:text-primary-light py-2 xm:py-1 px-2 rounded-l-xl">
-					<div className="flex flex-row items-left">
-						{screenType !== 'mobile' && getFieldIcon(fieldName)}
-						<span className="md:ml-1 flex items-center">{label}:</span>
-					</div>
-				</td>
-				<td className="text-gray-700 dark:text-white py-2 xm:py-1 px-2 rounded-r-xl">{fieldValue}</td>
-			</tr>
-		);
-	} else {
-		return null;
-	}
+const getValueByPath = (pathArray, source) => {
+	return pathArray.reduce((acc, key) => acc ? acc[key] : undefined, source);
 };
 
 const CredentialInfo = ({ parsedCredential, mainClassName = "text-sm lg:text-base w-full" }) => {
+	const { i18n } = useTranslation();
+	const { language, options: { fallbackLng } } = i18n;
 
-	const screenType = useScreenType();
+	const signedClaims = parsedCredential?.signedClaims;
+	const claims = parsedCredential?.metadata?.credential?.metadataDocuments?.[0]?.claims;
+
+	// Define custom claims to display from signedClaims if claims is missing
+	const customClaims = [
+		{ path: ['given_name'], display: [{ lang: 'en', label: 'Given Name' }] },
+		{ path: ['family_name'], display: [{ lang: 'en', label: 'Family Name' }] },
+		{ path: ['birth_date'], display: [{ lang: 'en', label: 'Birth Date' }] },
+		{ path: ['document_number'], display: [{ lang: 'en', label: 'Document Number' }] },
+		{ path: ['issuance_date'], display: [{ lang: 'en', label: 'Issuance Date' }] },
+		{ path: ['expiry_date'], display: [{ lang: 'en', label: 'Expiry Date' }] },
+	];
+
+	// Check each customClaims for existence in signedClaims before displaying
+	const existingCustomClaims = customClaims.filter(field => getValueByPath(field.path, signedClaims) !== undefined);
+
+	// Prepare display claims
+	const displayClaims = claims && Array.isArray(claims) ? claims : existingCustomClaims;
+
+	// Filter and prepare claims for display
+	const claimsWithValues = displayClaims.map(claim => {
+		const rawValue = getValueByPath(claim.path, signedClaims);
+		const { label, description } = getLabelAndDescriptionByLang(claim.display, language, fallbackLng);
+		if (rawValue && label) {
+			const formattedValue = typeof rawValue === 'boolean' ? String(rawValue) : formatDate(rawValue, 'date');
+			return {
+				label,
+				description,
+				value: formattedValue
+			};
+		}
+		return null;
+	}).filter(claim => claim !== null);
 
 	return (
-		<div className={mainClassName}>
-			<table className="lg:w-4/5">
-				<tbody className="divide-y-4 divide-transparent">
-					{parsedCredential.signedClaims && (
-						<>
-							{renderRow('expdate', 'Expiration', parsedCredential.signedClaims?.exp ? formatDate(new Date(parsedCredential.signedClaims?.exp * 1000).toISOString()) : null, screenType)}
-							{renderRow('familyName', 'Family Name', parsedCredential.signedClaims?.family_name, screenType)}
-							{renderRow('firstName', 'Given Name', parsedCredential.signedClaims?.given_name, screenType)}
-							{renderRow('id', 'Personal ID', parsedCredential.signedClaims?.personal_identifier, screenType)}
-							{renderRow('dateOfBirth', 'Birthday', formatDate(parsedCredential.signedClaims?.dateOfBirth, 'date'), screenType)}
-							{renderRow('dateOfBirth', 'Birthday', formatDate(parsedCredential.signedClaims?.birth_date, 'date'), screenType)}
-							{renderRow('diplomaTitle', 'Title', parsedCredential.signedClaims?.title, screenType)}
-							{renderRow('eqfLevel', 'EQF', parsedCredential.signedClaims?.eqf_level, screenType)}
-							{renderRow('grade', 'Grade', parsedCredential.signedClaims?.grade, screenType)}
-							{renderRow('id', 'Social Security Number', parsedCredential.signedClaims?.ssn, screenType)}
-							{renderRow('id', 'Document Number', parsedCredential.signedClaims?.document_number, screenType)}
-						</>
-					)}
-				</tbody>
-			</table>
+		<div className={mainClassName} data-testid="credential-info">
+			{claimsWithValues.length > 0 && (
+				<div>
+					{claimsWithValues.map((claim, index) => (
+						<p key={index} className='py-1 px-2'>
+							<span className='font-bold text-primary dark:text-primary-light'>
+								{claim.label}:{' '}
+							</span>
+							<span className="text-gray-700 dark:text-white">
+								{claim.value}
+							</span>
+						</p>
+					))}
+				</div>
+			)}
 		</div>
-	);
+	)
 };
 
 export default CredentialInfo;
