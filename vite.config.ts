069674cf13ba0acfbf88a71560d057fad8000436
gkatrakazas <grigoriskatrakazas@gmail.com>
2025-04-08 08:41:41 +0300

    added robots generation plugin

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { ManifestOptions, VitePWA } from 'vite-plugin-pwa';
 import fs from "fs";
 import path from "path";
 
-function generateManifest(env) : Partial<ManifestOptions>{
+function generateManifest(env): Partial<ManifestOptions> {
 
 	return {
 		"short_name": env.VITE_STATIC_NAME || 'wwWallet',
@@ -104,8 +104,47 @@ function ManifestPlugin(env) {
 	}
 }
 
-export default defineConfig(({mode}) => {
-	const env = loadEnv(mode ,process.cwd(), '')
+function generateRobotsTxt(env: Record<string, string>): string {
+	const baseUrl = env.VITE_STATIC_PUBLIC_URL || 'https://demo.wwwallet.org';
+	return `
+User-agent: *
+Disallow: /settings
+Disallow: /credential/
+Disallow: /history
+Disallow: /add
+Disallow: /send
+Allow: /login
+
+Allow: /
+Sitemap: ${baseUrl}/sitemap.xml
+`.trim();
+}
+
+function RobotsTxtPlugin(env) {
+	return {
+		name: 'robots-txt-plugin',
+
+		configureServer(server) {
+			const robotsPath = path.resolve("public/robots.txt");
+			fs.writeFileSync(robotsPath, generateRobotsTxt(env));
+
+			server.watcher.on("change", (file) => {
+				if (file.endsWith(".env")) {
+					console.log("Environment file changed. Regenerating robots.txt...");
+					fs.writeFileSync(robotsPath, generateRobotsTxt(env));
+				}
+			});
+		},
+
+		buildStart() {
+			const robotsPath = path.resolve("public/robots.txt");
+			fs.writeFileSync(robotsPath, generateRobotsTxt(env));
+		},
+	};
+}
+
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '')
 	return {
 		base: '/',
 		plugins: [
@@ -113,6 +152,7 @@ export default defineConfig(({mode}) => {
 			svgr(),
 			eslint(),
 			ManifestPlugin(env),
+			RobotsTxtPlugin(env),
 			VitePWA({
 				registerType: 'autoUpdate',
 				srcDir: 'src',
