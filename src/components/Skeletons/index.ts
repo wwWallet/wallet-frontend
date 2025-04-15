dbd40eb782472eb120d78f8195a89744b5d3ba3b
gkatrakazas <grigoriskatrakazas@gmail.com>
2025-04-15 12:25:59 +0300

    create Credential Card and Info Skeletons into Skeleltons folder

---
+++ src/components/Skeletons/index.ts
@@ -0,0 +1,7 @@
+/**
+ * @file index.ts
+ * @description Central export hub for all skeleton components used in loading states across the wallet.
+ */
+
+export { default as CredentialCardSkeleton } from './CredentialCardSkeleton';
+export { default as CredentialInfoSkeleton } from './CredentialInfoSkeleton';
