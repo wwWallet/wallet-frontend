6d956586dc35465efc6c967c5298d0343e61b52b
kkmanos <koukoularisman@gmail.com>
2025-05-13 17:51:27 +0300

    provided basic schema for re-usability

--- src/components/Popups/GenericConsentPopup.jsx
+++ src/components/Popups/GenericConsentPopup.jsx
@@ -52,11 +52,15 @@ function GenericConsentPopup({ popupConsentState, setPopupConsentState, showCons
 		<PopupLayout isOpen={popupConsentState?.isOpen} onClose={onClose} loading={false} fullScreen={screenType !== 'desktop'}>
 			<div className={`${screenType !== 'desktop' && 'pb-16'}`}>
 				<div>
+					{popupConsentState?.options?.title ? <h2 className="text-lg font-bold mb-2 text-primary dark:text-white">{popupConsentState?.options?.title}</h2> : <></> }
 					<hr className="mb-2 border-t border-primary/80 dark:border-white/80" />
-					<p className="text-gray-700 italic dark:text-white text-sm mt-2 mb-4">
-						{t('selectCredentialPopup.descriptionSelect')}
-					</p>
 
+					{popupConsentState?.options ? popupConsentState?.options?.attestations.map((a) => {
+						return (<>
+						<h4 className="text-lg font-bold mb-2 text-primary dark:text-white">{a.heading}</h4>
+						<p className="text-gray-700 italic dark:text-white text-sm mt-2 mb-4">{a.text}</p>
+					</>)
+					}): <></>}
 				</div>
 
 				<div className={`flex justify-between pt-4 z-10 ${screenType !== 'desktop' && 'fixed bottom-0 left-0 right-0 bg-white dark:bg-gray-800 flex px-6 pb-6 flex shadow-2xl rounded-t-lg w-auto'}`}>
