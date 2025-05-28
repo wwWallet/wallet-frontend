798f3ca56a49429d3d75d18668453a70f3425844
gkatrakazas <grigoriskatrakazas@gmail.com>
2025-05-28 14:53:47 +0300

    on dataset json div add max height, enable resize height and inprove style

--- src/components/Credentials/CredentialJson.jsx
+++ src/components/Credentials/CredentialJson.jsx
@@ -5,11 +5,11 @@ const CredentialJson = ({ parsedCredential }) => {
 	if (!parsedCredential?.signedClaims) return null;
 
 	return (
-		<div className='w-full'>
+		<div className='w-full py-2'>
 			{parsedCredential && (
-		<div className="dark:bg-gray-800 dark:text-white border rounded p-2 text-sm w-full rounded-xl">
-		<JsonViewer value={parsedCredential.signedClaims} />
-	</div>
+				<div className="h-60 resize-y overflow-auto min-h-32 bg-white dark:bg-gray-800 dark:text-white border rounded p-2 text-sm rounded-xl">
+					<JsonViewer value={parsedCredential.signedClaims} />
+				</div>
 			)}
 		</div>
 	);
