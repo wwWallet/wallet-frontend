dfa0aed12a7a5304b38541d32431ef5c21cfe031
Emil Lundberg <emil@yubico.com>
2025-09-08 15:32:15 +0200

    Test merging new_presentation events

--- src/services/WalletStateOperations.test.ts
+++ src/services/WalletStateOperations.test.ts
@@ -330,6 +330,48 @@ describe("The WalletStateOperations", () => {
 		}
 	});
 
+	it("mergeEventHistories de-duplicates new_presentation events by eventId.", async () => {
+		let container: WalletStateContainer = WalletStateOperations.initialWalletStateContainer();
+		container = await WalletStateOperations.addNewPresentationEvent(container, 0, "data0", [], 0, "");
+		container.events[0].timestampSeconds = 0;
+
+		const container1 = await WalletStateOperations.addNewPresentationEvent(container, 1, "data1", [], 0, "");
+		container1.events[1].timestampSeconds = 1;
+		let container2 = await WalletStateOperations.addNewPresentationEvent(container, 2, "data2a", [], 0, "");
+		container2.events[1].timestampSeconds = 2;
+		container2 = await WalletStateOperations.addNewPresentationEvent(container2, 3, "data2b", [], 0, "");
+		container2.events[2].timestampSeconds = 3;
+		container2.events[2].eventId = container1.events[1].eventId;
+
+		{
+			const mergedL = await WalletStateOperations.mergeEventHistories(container1, container2);
+			assert.deepEqual(mergedL, {
+				lastEventHash: container.lastEventHash,
+				S: container.S,
+				events: [
+					...container.events,
+					...container2.events.slice(1),
+				],
+			});
+		}
+
+		{
+			const mergedR = await WalletStateOperations.mergeEventHistories(container2, container1);
+			assert.deepEqual(mergedR, {
+				lastEventHash: container.lastEventHash,
+				S: container.S,
+				events: [
+					...container.events,
+					container1.events[1],
+					{
+						...container2.events[1],
+						parentHash: await WalletStateUtils.calculateEventHash(container1.events[1]),
+					},
+				],
+			});
+		}
+	});
+
 	it("should successfully fold one event at a time", async () => {
 		let container: WalletStateContainer = WalletStateOperations.initialWalletStateContainer();
 
