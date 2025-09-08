34f0ccb25ffe381f4cf78cfe7e71811b84169a13
Emil Lundberg <emil@yubico.com>
2025-09-08 16:20:12 +0200

    Test merging save_credential_issuance_session events

--- src/services/WalletStateOperations.test.ts
+++ src/services/WalletStateOperations.test.ts
@@ -451,6 +451,43 @@ describe("The WalletStateOperations", () => {
 		assert.deepEqual(mergedR, mergedL);
 	});
 
+	it("mergeEventHistories de-duplicates save_credential_issuance_session events by eventId.", async () => {
+		let container: WalletStateContainer = WalletStateOperations.initialWalletStateContainer();
+		container = await WalletStateOperations.addSaveCredentialIssuanceSessionEvent(container, 0, "iss0", "", "", "");
+		container.events[0].timestampSeconds = 0;
+
+		const container1 = await WalletStateOperations.addSaveCredentialIssuanceSessionEvent(container, 1, "iss1", "", "", "");
+		container1.events[1].timestampSeconds = 1;
+		let container2 = await WalletStateOperations.addSaveCredentialIssuanceSessionEvent(container, 2, "iss2a", "", "", "");
+		container2.events[1].timestampSeconds = 2;
+		container2 = await WalletStateOperations.addSaveCredentialIssuanceSessionEvent(container2, 3, "iss2b", "", "", "");
+		container2.events[2].timestampSeconds = 3;
+		container2.events[2].eventId = container1.events[1].eventId;
+
+		const mergedL = await WalletStateOperations.mergeEventHistories(container1, container2);
+		assert.deepEqual(mergedL, {
+			lastEventHash: container.lastEventHash,
+			S: container.S,
+			events: [
+				...container.events,
+				...container2.events.slice(1),
+			],
+		});
+		const mergedR = await WalletStateOperations.mergeEventHistories(container2, container1);
+		assert.deepEqual(mergedR, {
+			lastEventHash: container.lastEventHash,
+			S: container.S,
+			events: [
+				...container.events,
+				container1.events[1],
+				{
+					...container2.events[1],
+					parentHash: await WalletStateUtils.calculateEventHash(container1.events[1]),
+				},
+			],
+		});
+	});
+
 	it("should successfully fold one event at a time", async () => {
 		let container: WalletStateContainer = WalletStateOperations.initialWalletStateContainer();
 
