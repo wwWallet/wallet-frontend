5f9597281b3e355e3a6147e07122244723d931cc
Emmanouil Koukoularis <koukoularisman@gmail.com>
2025-09-08 13:43:19 +0300

    formatting fixes

--- src/lib/services/OpenID4VP/TransactionData/TransactionDataComponent.tsx
+++ src/lib/services/OpenID4VP/TransactionData/TransactionDataComponent.tsx
@@ -4,16 +4,16 @@ import { TransactionDataRequest } from "./TransactionDataRequest/TransactionData
 
 
 export const withTransactionData = (req: TransactionDataRequest) => {
-  const TransactionDataComponent: React.FC = () => {
-    return (
-      <div className="p-2 text-gray-700 text-sm dark:text-white mt-2">
-        <span className="text-primary text-sm font-bold dark:text-white block mb-1">
-          The verifier requested the acceptance of the example transaction data
-        </span>
-      </div>
-    );
-  };
+	const TransactionDataComponent: React.FC = () => {
+		return (
+			<div className="p-2 text-gray-700 text-sm dark:text-white mt-2">
+				<span className="text-primary text-sm font-bold dark:text-white block mb-1">
+					The verifier requested the acceptance of the example transaction data
+				</span>
+			</div>
+		);
+	};
 
-  TransactionDataComponent.displayName = "TransactionDataComponent";
-  return TransactionDataComponent;
+	TransactionDataComponent.displayName = "TransactionDataComponent";
+	return TransactionDataComponent;
 };
