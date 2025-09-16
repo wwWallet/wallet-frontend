581ce5346e3486e2362dd5a7d8e6d85eb33bbc20
Emil Lundberg <emil@yubico.com>
2025-09-16 14:09:55 +0200

    Fix type error in uint8ArrayToHexString
    Fixes the following type checking error:
    
    ```
    TypeCheckError: Expected 0 arguments, but got 1.
     ❯ src/services/keystore.ts:1330:98
        1328|  );
        1329|
        1330|  const uint8ArrayToHexString = (uint8Array: any) => Array.from(uint8Array, byte => byte.toString(16).padStart(2, '0')).join('');
           |                                                                                                  ^
        1331|  console.log("Session transcript bytes (HEX): ", uint8ArrayToHexString(new Uint8Array(sessionTranscriptBytes)));
        1332|
    ```

--- src/services/keystore.ts
+++ src/services/keystore.ts
@@ -1327,7 +1327,7 @@ export async function generateDeviceResponse([privateData, mainKey, calculatedSt
 		mdocGeneratedNonce
 	);
 
-	const uint8ArrayToHexString = (uint8Array: any) => Array.from(uint8Array, byte => byte.toString(16).padStart(2, '0')).join('');
+	const uint8ArrayToHexString = (uint8Array: Uint8Array) => Array.from(uint8Array, byte => byte.toString(16).padStart(2, '0')).join('');
 	console.log("Session transcript bytes (HEX): ", uint8ArrayToHexString(new Uint8Array(sessionTranscriptBytes)));
 
 	const deviceResponseMDoc = await DeviceResponse.from(mdocCredential)
