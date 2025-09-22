b5ef9c7d8f032e2d22b1c5212508be9aa20316ab
gkatrakazas <grigoriskatrakazas@gmail.com>
2025-09-22 13:45:22 +0300

    minor style padding fixes

--- src/components/Auth/PrivateRoute.tsx
+++ src/components/Auth/PrivateRoute.tsx
@@ -63,80 +63,82 @@ export function NotificationPermissionWarning(): React.ReactNode {
 	return (
 		show
 			? (
-				<div className="bg-orange-100 shadow-lg p-4 rounded-lg mb-4 flex items-center">
-					<div className="mr-4 text-orange-500">
-						<FaExclamationTriangle size={24} />
-					</div>
+				<div className="px-6 sm:px-12 w-full">
+					<div className="bg-orange-100 shadow-lg p-4 rounded-lg mb-4 flex items-center">
+						<div className="mr-4 text-orange-500">
+							<FaExclamationTriangle size={24} />
+						</div>
 
-					{isOnline === false && isMessageOfflineVisible === false && (
-						<>
-							<div className="flex-grow">
-								<p className='text-sm'>
-									<Trans
-										i18nKey="layout.messageOffline"
-										components={{ strong: <strong /> }}
-									/>
-								</p>
-							</div>
-							<button
-								id="close-message-offline"
-								className="ml-2 text-gray-800"
-								onClick={handleCloseMessageOffline}
-							>
-								<FaTimes size={24} />
-							</button>
-						</>
-					)}
-					{isOnline === true && (
-						<>
-							{!isPermissionGranted && (
-								<>
-									<div className="flex-grow">
-										<p className='text-sm'>
-											<Trans
-												i18nKey="layout.messageAllowPermission"
-												components={{ strong: <strong /> }}
-											/>
-										</p>
-									</div>
-									<button
-										id="close-message-no-granted"
-										className="ml-2 text-gray-800"
-										onClick={handleCloseMessageNoGranted}
-									>
-										<FaTimes size={24} />
-									</button>
-								</>
-							)}
-							{isPermissionGranted && tokenSentInSession === false && (
-								<>
-									<div className="flex-grow">
-										<p className='text-sm'>
-											<Trans
-												i18nKey="layout.messageResetPermission"
-												components={{
-													strong: <strong />,
-													reloadButton:
-														<button
-															id="reset-notification-permission"
-															className='text-primary underline'
-															onClick={() => window.location.reload()}
-														/>,
-												}}
-											/>
-										</p>
-									</div>
-									<button
-										id="close-message-granted"
-										className="ml-2 text-gray-800"
-										onClick={handleCloseMessageGranted}
-									>
-										<FaTimes size={24} />
-									</button>
-								</>
-							)}
-						</>
-					)}
+						{isOnline === false && isMessageOfflineVisible === false && (
+							<>
+								<div className="flex-grow">
+									<p className='text-sm'>
+										<Trans
+											i18nKey="layout.messageOffline"
+											components={{ strong: <strong /> }}
+										/>
+									</p>
+								</div>
+								<button
+									id="close-message-offline"
+									className="ml-2 text-gray-800"
+									onClick={handleCloseMessageOffline}
+								>
+									<FaTimes size={24} />
+								</button>
+							</>
+						)}
+						{isOnline === true && (
+							<>
+								{!isPermissionGranted && (
+									<>
+										<div className="flex-grow">
+											<p className='text-sm'>
+												<Trans
+													i18nKey="layout.messageAllowPermission"
+													components={{ strong: <strong /> }}
+												/>
+											</p>
+										</div>
+										<button
+											id="close-message-no-granted"
+											className="ml-2 text-gray-800"
+											onClick={handleCloseMessageNoGranted}
+										>
+											<FaTimes size={24} />
+										</button>
+									</>
+								)}
+								{isPermissionGranted && tokenSentInSession === false && (
+									<>
+										<div className="flex-grow">
+											<p className='text-sm'>
+												<Trans
+													i18nKey="layout.messageResetPermission"
+													components={{
+														strong: <strong />,
+														reloadButton:
+															<button
+																id="reset-notification-permission"
+																className='text-primary underline'
+																onClick={() => window.location.reload()}
+															/>,
+													}}
+												/>
+											</p>
+										</div>
+										<button
+											id="close-message-granted"
+											className="ml-2 text-gray-800"
+											onClick={handleCloseMessageGranted}
+										>
+											<FaTimes size={24} />
+										</button>
+									</>
+								)}
+							</>
+						)}
+					</div>
 				</div>
 			)
 			: <></>
