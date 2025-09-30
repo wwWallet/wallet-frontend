38fe6da0cbd0e293160137d714e55a5721ae1a60
gkatrakazas <grigoriskatrakazas@gmail.com>
2025-09-30 12:05:26 +0300

    move higher the I18nextProvider

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -1,5 +1,11 @@
 // AppProvider.tsx
 import React, { ReactNode } from 'react';
+
+// Import i18next and set up translations
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
+
+// Contexts
 import { StatusContextProvider } from './context/StatusContextProvider';
 import { SessionContextProvider } from './context/SessionContextProvider';
 import { CredentialsContextProvider } from './context/CredentialsContextProvider';
@@ -9,6 +15,7 @@ import { AppSettingsProvider } from './context/AppSettingsProvider';
 import { NotificationProvider } from './context/NotificationProvider';
 import { NativeWrapperProvider } from './context/NativeWrapper';
 
+// Hocs
 import UriHandler from './hocs/UriHandler';
 
 type RootProviderProps = {
@@ -20,19 +27,21 @@ const AppProvider: React.FC<RootProviderProps> = ({ children }) => {
 		<StatusContextProvider>
 			<SessionContextProvider>
 				<CredentialsContextProvider>
-					<OpenID4VPContextProvider>
-						<OpenID4VCIContextProvider>
-							<UriHandler>
-								<AppSettingsProvider>
-									<NotificationProvider>
-										<NativeWrapperProvider>
-											{children}
-										</NativeWrapperProvider>
-									</NotificationProvider>
-								</AppSettingsProvider>
-							</UriHandler>
-						</OpenID4VCIContextProvider>
-					</OpenID4VPContextProvider>
+					<I18nextProvider i18n={i18n}>
+						<OpenID4VPContextProvider>
+							<OpenID4VCIContextProvider>
+								<UriHandler>
+									<AppSettingsProvider>
+										<NotificationProvider>
+											<NativeWrapperProvider>
+												{children}
+											</NativeWrapperProvider>
+										</NotificationProvider>
+									</AppSettingsProvider>
+								</UriHandler>
+							</OpenID4VCIContextProvider>
+						</OpenID4VPContextProvider>
+					</I18nextProvider>
 				</CredentialsContextProvider>
 			</SessionContextProvider>
 		</StatusContextProvider>
