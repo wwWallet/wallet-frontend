71f8579a3e43642d6d017eaf8a4d104194742bea
gkatrakazas <grigoriskatrakazas@gmail.com>
2025-10-07 11:27:44 +0300

    move ans fix presentation details into HistoryDetailContent

--- src/components/History/HistoryDetailContent.jsx
+++ src/components/History/HistoryDetailContent.jsx
@@ -1,5 +1,6 @@
 // External libraries
 import React, { useContext, useEffect, useState } from 'react';
+import { VerifierIcon } from '@/assets/images/verifier_icon';
 
 // Components
 import Slider from '../Shared/Slider';
@@ -11,6 +12,9 @@ import useScreenType from '../../hooks/useScreenType';
 import { useHttpProxy } from '@/lib/services/HttpProxy/HttpProxy';
 import { CredentialVerificationError } from "wallet-common/dist/error";
 import { VerifiableCredentialFormat } from "wallet-common/dist/types";
+
+import prettyDomain from '@/utils/prettyDomain';
+import { formatDate } from '@/functions/DateFormat';
 
 const HistoryDetailContent = ({ historyItem }) => {
 	const [currentSlide, setCurrentSlide] = React.useState(1);
@@ -28,7 +32,14 @@ const HistoryDetailContent = ({ historyItem }) => {
 
 	return (
 		<div className="py-2 w-full">
-			<div className="">
+			<div className='flex items-center gap-2 px-2 mb-4'>
+				<VerifierIcon size={40} className="text-white bg-primary dark:bg-primary-light p-2 rounded-md shrink-0" />
+				<div>
+					<p className='text-md font-semibold text-gray-700 dark:text-white'>{prettyDomain(historyItem[0].presentation.audience)} </p>
+					<p className='text-sm text-gray-700 dark:text-gray-300'>{formatDate(historyItem[0].presentation.presentationTimestampSeconds)}</p>
+				</div>
+			</div>
+			<div>
 				<Slider
 					items={historyItem} // note: a HistoryItem is an array of presentations that happened in a single OpenID4VP transaction
 					renderSlideContent={renderSlideContent}
--- src/pages/History/HistoryDetail.jsx
+++ src/pages/History/HistoryDetail.jsx
@@ -2,7 +2,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { FaArrowLeft } from "react-icons/fa";
-import { VerifierIcon } from '@/assets/images/verifier_icon';
 import { useTranslation } from 'react-i18next';
 
 // Contexts
@@ -50,26 +49,6 @@ const HistoryDetail = () => {
 						<FaArrowLeft size={20} className="text-2xl text-primary dark:text-white" />
 					</button>
 					<H1 heading={t('pageHistory.presentationDetails.title')} hr={false} />
-				</div>
-				<div className="flex flex-col lg:flex-row lg:mt-5 mt-0">
-					<div className='flex flex-row'>
-						<div className='flex flex-col items-left gap mt-2 px-2'>
-							{history.length > 0 && (
-								<>
-									<div className='flex items-center gap-2'>
-										<div className='my-2'>
-											<VerifierIcon className="fill-white bg-primary dark:bg-primary-light p-2 w-12 rounded-md" />
-										</div>
-										<div>
-											<p className='text-lg font-bold text-primary dark:text-white'>{Object.values(history)[0].audience} </p>
-											<p className='text-sm text-gray-700 dark:text-gray-300'>{formatDate(Object.values(history)[0].presentationTimestampSeconds)}</p>
-										</div>
-									</div>
-								</>
-							)}
-						</div>
-					</div>
-
 				</div>
 				{selectedHistoryItem.length > 0 && (
 					<HistoryDetailContent historyItem={selectedHistoryItem} />
