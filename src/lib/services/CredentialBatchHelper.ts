155c0a083ee0fdf2d09646381ebba49d2341227e
Emmanouil Koukoularis <koukoularisman@gmail.com>
2025-10-10 13:40:09 +0300

    fix: more reliable way to derive the least used credential during presentation

--- src/lib/services/CredentialBatchHelper.ts
+++ src/lib/services/CredentialBatchHelper.ts
@@ -1,9 +1,17 @@
-import { compareBy } from "../../util";
+import { WalletState, WalletStateCredential } from "@/services/WalletStateSchemaVersion1";
+import { compareBy, reverse } from "../../util";
 import { ExtendedVcEntity } from "@/context/CredentialsContext";
 
 
-export async function getLeastUsedCredentialInstance(batchId: number, cList: ExtendedVcEntity[]) {
-	const credsByBatchId = cList.filter((c) => c.batchId === batchId);
-	credsByBatchId.sort(compareBy((c) => c.sigCount));
-	return credsByBatchId[0];
+export async function getLeastUsedCredentialInstance(batchId: number, cList: ExtendedVcEntity[], walletState: WalletState): Promise<WalletStateCredential | null> {
+	const credByBatchId = cList.filter((c) => c.batchId === batchId)[0];
+	const instances = credByBatchId.instances;
+	instances.sort(compareBy((c) => c.sigCount));
+	const leastUsedInstance = instances[0];
+	const { instanceId } = leastUsedInstance;
+	const credential = walletState.credentials.filter((c) => c.batchId === batchId && c.instanceId === instanceId)[0];
+	if (!credential) {
+		return null;
+	}
+	return credential;
 }
