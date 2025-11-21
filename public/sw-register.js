e6f66545402587811d0ddc889ea780506b1dbc14
Yorgos <dr.george995@gmail.com>
2025-11-21 18:13:06 +0200

    identation fix

--- public/sw-register.js
+++ public/sw-register.js
@@ -1,25 +1,24 @@
-
 const tt = window.trustedTypes || window.TrustedTypes;
 
 const swPolicy = tt
-	  ? tt.createPolicy('sw-register', {
-		  createScriptURL(url) {
-			  if (url === '/service-worker.js') {
-				  return url;
-			  }
-			  throw new TypeError('Untrusted service worker URL blocked by Trusted Types policy');
-		  }
-	  })
-	  : null;
+	? tt.createPolicy('sw-register', {
+		createScriptURL(url) {
+			if (url === '/service-worker.js') {
+				return url;
+			}
+			throw new TypeError('Untrusted service worker URL blocked by Trusted Types policy');
+		}
+	})
+	: null;
 
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
 		const swUrl = '/service-worker.js';
 		const trustedSwUrl = swPolicy ? swPolicy.createScriptURL(swUrl) : swUrl;
 		navigator.serviceWorker
-				 .register(trustedSwUrl, { scope: '/' })
-				 .catch(err => {
-					 console.error('Service worker registration failed:', err);
-				 });
+			.register(trustedSwUrl, { scope: '/' })
+			.catch(err => {
+				console.error('Service worker registration failed:', err);
+			});
 	});
 }
