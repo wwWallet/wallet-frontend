ed21a40792e975efdd4fb0fd97b77a577185cf46
Simon <contact@smn.codes>
2026-02-12 12:55:00 +0100

    format: stick to single quotes

--- config/files/metadata-image.ts
+++ config/files/metadata-image.ts
@@ -1,19 +1,19 @@
-import path from "node:path";
-import { writeFile } from "node:fs/promises";
-import { MetadataImage } from "../../branding";
-import { ConfigMap } from "../utils/config";
+import path from 'node:path';
+import { writeFile } from 'node:fs/promises';
+import { MetadataImage } from '../../branding';
+import { ConfigMap } from '../utils/config';
 
 /**
  * Generates a metadata image based on the provided configuration and writes it to the specified destination directory.
  */
 export default async function metadataImage(destDir: string, config: ConfigMap) {
-	const fileName = "image.png";
+	const fileName = 'image.png';
 
 	const generationConfig = {
 		title: config.META_STATIC_NAME || 'wwWallet',
 	};
 
-	const cacheDir = path.resolve("config", ".cache");
+	const cacheDir = path.resolve('config', '.cache');
 
 	if (!(await MetadataImage.hasFontsEnvironment(cacheDir))) {
 		await MetadataImage.setupFontsEnvironment(cacheDir);
