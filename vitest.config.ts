8678d92afc0d8fcd7df0702b75797837bc72447d
Simon <contact@smn.codes>
2026-02-12 16:21:47 +0100

    fix: restore vitest.config.ts

---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
+
+export default defineConfig({
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, './src'),
+		},
+	},
+	test: {
+		globals: true,
+		setupFiles: ['./setup-vitest.ts'],
+		exclude: ['lib/**', 'node_modules/**'],
+		environmentMatchGlobs: [
+			['**/services/*.test.ts', 'node'],
+			['**', 'jsdom']
+		],
+		typecheck: {
+			enabled: true,
+		},
+	},
+});
