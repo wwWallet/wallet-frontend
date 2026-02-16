45f0ac7e780771078662f1a0872af7e4a0b653fe
Didr <9605751+Didr@users.noreply.github.com.>
2026-02-16 12:10:11 +0100

    fix: use uppercase for http get method when using proxy

--- src/lib/services/HttpProxy/HttpProxy.ts
+++ src/lib/services/HttpProxy/HttpProxy.ts
@@ -143,7 +143,7 @@ export function useHttpProxy(): IHttpProxy {
 						response = await axios.post(`${walletBackendServerUrl}/proxy`, {
 							headers,
 							url,
-							method: 'get',
+							method: 'GET',
 						}, {
 							timeout: TIMEOUT,
 							headers: {
