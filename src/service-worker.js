3102bbca6e93bde0b5225061657a18ab8b5fd48f
gkatrakazas <grigoriskatrakazas@gmail.com>
2026-02-27 12:19:28 +0200

    import missing CacheFirst

--- src/service-worker.js
+++ src/service-worker.js
@@ -4,7 +4,7 @@ import { clientsClaim } from "workbox-core";
 import { ExpirationPlugin } from "workbox-expiration";
 import { precacheAndRoute, createHandlerBoundToURL, cleanupOutdatedCaches, } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
-import { StaleWhileRevalidate } from "workbox-strategies";
+import { StaleWhileRevalidate, CacheFirst } from "workbox-strategies";
 
 clientsClaim();
 
