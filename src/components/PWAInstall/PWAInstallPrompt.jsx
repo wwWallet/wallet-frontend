84f710119c10f345f979bfdc5a645d29f692225d
gkatrakazas <grigoriskatrakazas@gmail.com>
2026-05-05 17:36:41 +0300

    fix: add PWA install prompt icon

--- src/components/PWAInstall/PWAInstallPrompt.jsx
+++ src/components/PWAInstall/PWAInstallPrompt.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import '@khmyznikov/pwa-install';
 import { BASE_PATH } from '@/config';
 
-const manifestUrl = `${BASE_PATH.replace(/\/?$/, '/')}manifest.json`;
+const basePath = BASE_PATH.replace(/\/?$/, '/');
+const manifestUrl = `${basePath}manifest.json`;
+const iconUrl = `${basePath}icons/icon-64x64.png`;
 
 const PWAInstallPrompt = () => {
 	return (
 		<pwa-install
+			icon={iconUrl}
 			manifest-url={manifestUrl}
 			use-local-storage
 		/>
