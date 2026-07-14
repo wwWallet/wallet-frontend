226cd6d8548da1ff3a37dd5137709f37be16bab5
gkatrakazas <grigoriskatrakazas@gmail.com>
2026-07-14 15:15:56 +0300

    style:minor padding fix

--- src/components/Auth/AuthCard.tsx
+++ src/components/Auth/AuthCard.tsx
@@ -27,7 +27,7 @@ const AuthCard = ({
 			appHeading ?? <span className="text-primary dark:text-brand-light"> {t('common.walletName')}</span>
 		}>
 			<div className="p-8 sm:px-12 space-y-4 md:space-y-6 lg:space-y-8 bg-white rounded-lg dark:bg-dm-gray-900 border border-lm-gray-400 dark:border-dm-gray-600">
-				<h1 className="pt-4 text-xl font-bold leading-tight tracking-tight text-dm-gray-900 md:text-2xl text-center dark:text-white">
+				<h1 className="text-xl font-bold leading-tight tracking-tight text-dm-gray-900 md:text-2xl text-center dark:text-white">
 					{heading}
 				</h1>
 
