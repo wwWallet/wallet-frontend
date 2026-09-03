3955cddda81decbefda8e6af90d8332c7f2b0a58
Vasileios Markopoulos <vmarkop@gunet.gr>
2026-09-03 12:15:49 +0300

    Remove 'not attested' authenticator warning

--- src/pages/Settings/components/WebauthnCredentialItem.tsx
+++ src/pages/Settings/components/WebauthnCredentialItem.tsx
@@ -200,15 +200,7 @@ const WebauthnCredentialItem = ({
 					</div>
 					<div>
 						<p className="text-sm text-lm-gray-700 dark:text-dm-gray-300">{t('pageSettings.passkeyItem.authenticatorName')}</p>
-						<div className="flex flex-wrap items-center gap-2">
-							<p className="text-lm-gray-900 dark:text-white">{credential.authenticatorName || t("pageSettings.passkeyItem.unknownAuthenticator")}</p>
-							{credential.authenticatorName && (
-								<span className="inline-flex shrink-0 px-2 py-0.5 rounded-full text-xs font-normal bg-lm-orange/20 text-lm-gray-900 dark:bg-dm-orange/70 dark:text-white">
-									{t('pageSettings.passkeyItem.notAttested')}
-								</span>
-							)}
-
-						</div>
+						<p className="text-lm-gray-900 dark:text-white">{credential.authenticatorName || t("pageSettings.passkeyItem.unknownAuthenticator")}</p>
 					</div>
 				</div>
 
