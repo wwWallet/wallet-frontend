c0daeecc27e02ae530b86b07275666a275f1d282
gkatrakazas <grigoriskatrakazas@gmail.com>
2026-09-11 09:35:45 +0300

    chore(eslint): enable no-useless-escape and fix existing violation

--- eslint.config.js
+++ eslint.config.js
@@ -77,7 +77,6 @@ export default tseslint.config(
 			],
 			'@typescript-eslint/no-unsafe-function-type': 'off',
 			'no-empty': 'off',
-			'no-useless-escape': 'off',
 			'no-useless-catch': 'off',
 			'no-var': 'off',
 			'prefer-const': 'off',
