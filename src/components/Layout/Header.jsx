0daafb7e6f651dcc1f1e145d43fdaf705584c345
gkatrakazas <grigoriskatrakazas@gmail.com>
2026-09-22 15:54:27 +0300

    show mobile branding only on home page

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useLocation } from 'react-router-dom';
 import Logo from '../Logo/Logo';
 
 const Header = () => {
 	const { t } = useTranslation();
+	const { pathname } = useLocation();
+
+	if (pathname !== '/') return null;
 
 	return (
 		<header className="sticky top-0 z-50 w-full bg-inherit text-inherit flex items-center md:hidden border-b border-lm-gray-400 dark:border-dm-gray-600 transition-all duration-300 py-3 px-6">
